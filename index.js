c0f9f166c4f8dc9f8f6618810ce25d9113aa4f48
Perevechenkov Vladimir <48237694+DesperateHiking@users.noreply.github.com>
2019-10-06 23:41:20 +0500

    Add files via upload

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
-/**
- * @param array – массив элементов. Например, [1, 8, 3]
- * @param number – число, которое нужно прибавить к числу, полученному из массива
-*/
 function addNumber(array, number) {
     let sum = '';
+    let resArray = new Array();
     for (let i = 0; i < arr.length; i++) {
         if (Number.isInteger(arr[i]) && arr[i].toString().length === 1)
             sum += arr[i].toString();
         else return console.log('null');
     }
-    sum = parseInt(sum) + number;
-    console.log(sum);
+    sum = (parseInt(sum) + number).toString();
+    for (let i = 0; i < sum.length; i++) {
+        resArray[i] = sum[i];
+    }
+    return resArray;
 }
 
 module.exports = {
