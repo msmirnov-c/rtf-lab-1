649de85845ffc54f91e422077391a4a18f224672
Perevechenkov Vladimir <48237694+DesperateHiking@users.noreply.github.com>
2019-10-07 00:37:46 +0500

    Add files via upload

--- index.js
+++ index.js
@@ -1,17 +1,16 @@
 function addNumber(array, number) {
-    /*let sum = '';
+    let sum = '';
     let resArray = new Array();
-    for (let i = 0; i < arr.length; i++) {
-        if (Number.isInteger(arr[i]) && arr[i].toString().length === 1)
-            sum += arr[i].toString();
+    for (let i = 0; i < array.length; i++) {
+        if (Number.isInteger(array[i]) && array[i].toString().length === 1)
+            sum += array[i].toString();
         else return null;
     }
     sum = (parseInt(sum) + number).toString();
     for (let i = 0; i < sum.length; i++) {
         resArray[i] = Number.parseInt(sum[i]);
     }
-    return resArray;*/
-    return null;
+    return resArray;
 }
 
 module.exports = {
