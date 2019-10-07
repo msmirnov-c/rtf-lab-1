a2e874c253c7fdfc066f55c360a200711a253085
Desperate Hiking <perevechenkov1999@gmail.com>
2019-10-07 13:45:56 +0500

    Добавил отступы, использовал map

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 function addNumber(array, number) {
     let sum = '';
-    let resArray = new Array();
+    let resultArray = [];
+
     for (let e of array) {
         if (Number.isInteger(e) && e.toString().length === 1)
             sum += e.toString();
         else return null;
     }
+
     sum = (parseInt(sum) + number).toString();
-    for (let i = 0; i < sum.length; i++) {
-        resArray[i] = Number.parseInt(sum[i], 10);
-    }
-    return resArray;
+    resultArray = sum.split('').map(Number);
+    return resultArray;
 }
 
 module.exports = {
