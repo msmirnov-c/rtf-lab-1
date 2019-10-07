2b5461a478ca35870d4f3e165b743324450e239e
dartrhevan <vovasatunkin@mail.ru>
2019-10-07 19:38:07 +0500

    codestyle: change "do while" into "while"

--- index.js
+++ index.js
@@ -3,21 +3,22 @@
  * @param number – число, которое нужно прибавить к числу, полученному из массива
  */
 function addNumber(array, number) {
+    if(array.length <= 0 && number == 0) return  [0];
     let base = 0;
-    for(let i = 0; i < array.length; ++i) {
+    for(let i = 0; i < array.length; i++) {
         let digit = array[i];
         if(!Number.isInteger(digit) || digit > 9 || digit < 0)
             return null;
-        base += Math.pow(10, array.length - i - 1) * digit;
+        base += 10 **  (array.length - i - 1) * digit;
     }
-    let resultNumber = base + number, result = [];
-    do {
+    let resultNumber = base + number;
+    let result = [];
+    while(resultNumber > 0) {
         let digit = resultNumber % 10;
         resultNumber -= digit;
         resultNumber /= 10;
         result.unshift(digit);
     }
-    while(resultNumber > 0);
     return result;
 }
 
