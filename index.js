735c0bd1ff0dfe1dcd0f0e8053e0a21f40248800
dartrhevan <vovasatunkin@mail.ru>
2019-10-07 18:55:34 +0500

    local tests passed

--- index.js
+++ index.js
@@ -1,9 +1,26 @@
 /**
  * @param array – массив элементов. Например, [1, 8, 3]
  * @param number – число, которое нужно прибавить к числу, полученному из массива
-*/
+ */
 function addNumber(array, number) {
-    // Решение задачи
+    let base = 0;
+    for(let i = 0; i < array.length; ++i)
+    {
+        let digit = array[i];
+        if(!Number.isInteger(digit) || digit > 9 || digit < 0)
+            return null;
+        base += Math.pow(10, array.length - i - 1) * digit;
+    }
+    let resultNumber = base + number, result = [];
+    do
+    {
+        let digit = resultNumber % 10;
+        resultNumber -= digit;
+        resultNumber /= 10;
+        result.unshift(digit);
+    }
+    while(resultNumber > 0);
+    return result;
 }
 
 module.exports = {
