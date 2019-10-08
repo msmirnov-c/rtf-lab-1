afa5d7d2ac386481c23f1ce17349bdf7a05637e4
OvchinnikovJMA <47637806+OvchinnikovJMA@users.noreply.github.com>
2019-10-09 00:24:01 +0500

    Добавил решение
    Функция принимает массив чисел и число. Далее каждое число этого массива сверяется с условием. Если это не цифры от 0 до 9, то функция возвращает null. Далее каждый элемент массива преобразуется в строку и попадает в пустую строку и после эта строка преобразуется в число, к которому прибавляют исходное число, переданное в функцию. Далее число преобразуется в массив из цифр и этот массив является результатом вывода функции.

--- index.js
+++ index.js
@@ -3,9 +3,30 @@
  * @param number – число, которое нужно прибавить к числу, полученному из массива
 */
 function addNumber(array, number) {
-    // Решение задачи
+    let str = '', arr = [];
+    for (let i of array) {
+        if (isNaN(i) || i > 9 || i < 0 || i === null || i === Infinity)
+            return null;
+        str += i.toString();
+    }
+    str = parseInt(str);
+    str += number;
+    for (let i of str.toString()){
+        arr.push(parseInt(i));
+    }
+    return arr;
+    
 }
-
+/*let array = [1, 2, 4];
+const number = 1;
+array = addNumber(array, number);
+if(array != null){
+    for (let i of array){
+        console.log(i);
+    }
+}
+else console.log('sosat');
+*/
 module.exports = {
     addNumber
 };
