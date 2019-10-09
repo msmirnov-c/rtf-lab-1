da85a7afdf44a1fcf8366e151f0e41a975c86901
dartrhevan <vovasatunkin@mail.ru>
2019-10-09 12:47:25 +0500

    добавил отступы, const и while

--- index.js
+++ index.js
@@ -3,22 +3,26 @@
  * @param number – число, которое нужно прибавить к числу, полученному из массива
  */
 function addNumber(array, number) {
+    if(array.length == 0 && number == 0) return [0];
     let base = 0;
+
     for(let i = 0; i < array.length; i++) {
-        let digit = array[i];
+        const digit = array[i];
         if(!Number.isInteger(digit) || digit > 9 || digit < 0)
             return null;
         base += 10 **  (array.length - i - 1) * digit;
     }
+
     let resultNumber = base + number;
-    let result = [];
-    for(;;) {
+    const result = [];
+
+    while(resultNumber > 0) {
         let digit = resultNumber % 10;
         resultNumber -= digit;
         resultNumber /= 10;
         result.unshift(digit);
-        if(resultNumber <= 0) break;
     }
+
     return result;
 }
 
