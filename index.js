8601b08fc5c462842eca00ccfb3d2d9c5b2b91f9
M1M0G <M1M0G@github.com>
2019-10-10 00:30:16 +0500

    Update index.js

--- index.js
+++ index.js
@@ -13,8 +13,8 @@ function addNumber(array, number)
 
     if (lengthOfElem == true || typeOfElem == true || arrayIsExist == false )
     {
-      for(let i = 0; i < array.length; i++)
-        if (Math.floor(array[i]) !== i) throw null;
+        for(let i = 0; i < array.length; i++)
+          if (Math.floor(array[i]) !== i) throw null;
 
         num = array.join();
         num = Number(num.replace(/,/g, ''));
