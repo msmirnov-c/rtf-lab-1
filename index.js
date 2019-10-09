234d5952dae191e86f7ff4bf32f16f7eee4464a0
Maria <48256725+mashaartemeva@users.noreply.github.com>
2019-10-10 01:08:04 +0500

    Update index.js
    Очень кратко:
    Создала массив с допустимыми значениями входного массива. С помощью функции .indexOf() проверяю все ли значения во входном массиве есть в мною созданном. Если нет какого-либо значения, выводится null, если все норм, выводится требуемый результат( с помощью двух нехитрых циклов).

--- index.js
+++ index.js
@@ -3,7 +3,20 @@
  * @param number – число, которое нужно прибавить к числу, полученному из массива
 */
 function addNumber(array, number) {
-    // Решение задачи
+    let arr = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+    let res = 0;
+    let resArr = [];
+    for (let i = 0; i < array.length; i++) {
+        if (arr.indexOf(array[i]) == -1) return null;
+        res += (Math.pow(10, array.length - i - 1) * array[i]);
+    }
+    res += number;
+    while (res>=0.1) {
+        resArr.unshift(res % 10);
+        res -= res % 10;
+        res /= 10;
+    }
+    return resArr;
 }
 
 module.exports = {
