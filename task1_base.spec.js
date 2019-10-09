3da50703f6d2536406ce11846bb227ae13783606
dartrhevan <vovasatunkin@mail.ru>
2019-10-09 12:59:29 +0500

    codestyle: ещё один отступ

--- task1_base.spec.js
+++ task1_base.spec.js
@@ -2,20 +2,7 @@ const {addNumber} = require('./index');
 
 const cases = [
     { array: [1, 2, 4], number: 1, expected: [1, 2, 5] },
-    { array: [null], number: 125, expected: null },
-    { array: [1, 0, 9], number: 2, expected: [1, 1, 1] },
-    { array: [2, 5, 1], number: 5, expected: [2, 5, 6] },
-    { array: [1], number: 4020, expected:[4, 0, 2, 1]},
-    { array: [1, '4'],  number: 1, expected: null },
-    { array: [1, 25],  number: 1, expected: null },
-    { array: [-1, 2],  number: 71, expected: null },
-    { array: [1, 2.5],  number: 1, expected: null },
-    { array: [2, 5, 1], number: 54, expected: [3, 0, 5] },
-    { array: [], number: 0, expected: [0] },
-    { array: [5], number: 0, expected: [5] },
-    { array: [], number: 5, expected: [5] },
-    { array: [7], number: 0, expected: [7] },
-    { array: [7, 8], number: 0, expected: [7, 8] }
+    { array: [null], number: 125, expected: null }
 ];
 
 describe('add number', function () {
