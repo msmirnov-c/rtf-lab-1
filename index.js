e591b51e578747e2da3e9618b94ad34cdcd5dde4
Вершинин Никита <37085285+Netwar001@users.noreply.github.com>
2019-10-09 18:21:11 +0500

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,22 @@
  * @param number – число, которое нужно прибавить к числу, полученному из массива
 */
 function addNumber(array, number) {
-    // Решение задачи
+    let counter = 0;
+    for (let i = 0; i < array.length; i++) {
+        if (typeof (array[i]) != typeof (+array[i]))
+            counter++;
+    }
+    if (counter == 0) {
+        let mas = [];
+        let value = array.join();
+        value = Number(value.replace(/\,/g, ''));
+        value += number;
+        for (let i = 0; i < value.toString().length; i++)
+            mas.push(+value.toString().charAt(i));
+        console.log(mas);
+    }
+    else
+        console.log(NULL);
 }
 
 module.exports = {
