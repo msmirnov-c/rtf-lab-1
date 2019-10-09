d174d15a427f3f1fe0337bbbb85d58f671a7916c
M1M0G <M1M0G@github.com>
2019-10-09 22:23:30 +0500

    Update index.js

--- index.js
+++ index.js
@@ -4,24 +4,24 @@
 */
 function addNumber(array, number)
 {
-  var degree = array.length;
-  var num = 0;
-  var lengthOfElem = array.every(item => item.length === 1);// Решил использовать флаги для того чтобы лучше разобраться с работой every
-  var typeOfElem = array.every(item => typeof item === 'number'); //Проверка на тип данных
-  if (lengthOfElem == true && typeOfElem == true)
-  {
-    for(let i = 0; i < array.length; i++) // из массива в число
+    var degree = array.length;
+    var num = 0;
+    var lengthOfElem = array.every(item => item.length === 1);// Решил использовать флаги для того чтобы лучше разобраться с работой every
+    var typeOfElem = array.every(item => typeof item === 'number'); //Проверка на тип данных
+    if (lengthOfElem == true && typeOfElem == true)
     {
-      num+=(array[i]*Math.pow(10,degree));
-      degree--;
+        for(let i = 0; i < array.length; i++) // из массива в число
+        {
+            num+=(array[i]*Math.pow(10,degree));
+            degree--;
+        }
+        array = []; // отчистка старого массива
+        num = number + num; // сложили новое число
+        for (let i of num.toString())
+            array.push(parseInt(i));
     }
-      array = []; // отчистка старого массива
-      num = number + num; // сложили новое число
-      for (let i of num.toString())
-        array.push(parseInt(i));
-  }
-  else return null;// не подходит по усовию
-  return array;// возвращаю массив
+    else return null;// не подходит по усовию
+    return array;// возвращаю массив
 }
 
 module.exports = {
