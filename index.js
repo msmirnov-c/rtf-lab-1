519e7b7cc5ceef2d6fac9d4be6e1bd3f38d3035c
M1M0G <M1M0G@github.com>
2019-10-10 00:58:55 +0500

    Update index.js

--- index.js
+++ index.js
@@ -9,19 +9,18 @@ function addNumber(array, number)
     let newArray = [];
     var lengthOfElem = array.every(item => item.length === 1);// Решил использовать флаги для того чтобы лучше разобраться с работой every
     var typeOfElem = array.every(item => typeof item === 'number');//Проверка на тип данных
-    var integer = array.every(item => item.isInteger);
+    var integer = array.every(item => Number.isInteger(item));
     var arrayIsExist = isNaN(array);// Проверка на существование массива
 
     if (lengthOfElem == true || typeOfElem == true || arrayIsExist == false || integer == true)
     {
         for(let i = 0; i < array.length; i++)
-            if (array[i].isInteger == false) throw null;
+            if (array[i].isInteger == false || number.isInteger == false) throw null;
         num = array.join();
         num = Number(num.replace(/,/g, ''));
         num += number;
         newArray = num.toString(10).split('').map(Number);
-        //for (let i of num.toString(10))
-        //  newArray.push(parseInt(i));
+
     }
     else return null;// не подходит по усовию
     return newArray;// возвращаю массив
@@ -40,3 +39,5 @@ module.exports = {
 //  }
 //  num = number + num; // сложили новое число
 //    if (Math.floor(array[i]) !== i) throw null;
+//for (let i of num.toString(10))
+//  newArray.push(parseInt(i));
