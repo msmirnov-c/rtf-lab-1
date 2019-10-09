64d48aa6be658d9391da2e5141eeba4627b43562
M1M0G <M1M0G@github.com>
2019-10-09 22:19:03 +0500

    Update index.js

--- index.js
+++ index.js
@@ -8,21 +8,17 @@ function addNumber(array, number)
   var num = 0;
   var lengthOfElem = array.every(item => item.length === 1);// Решил использовать флаги для того чтобы лучше разобраться с работой every
   var typeOfElem = array.every(item => typeof item === 'number'); //Проверка на тип данных
-
-
   if (lengthOfElem == true && typeOfElem == true)
   {
-      for(let i = 0; i < array.length; i++) // из массива в число
-      {
-        num+=(array[i]*Math.pow(10,degree));
-        degree--;
-      }
-
+    for(let i = 0; i < array.length; i++) // из массива в число
+    {
+      num+=(array[i]*Math.pow(10,degree));
+      degree--;
+    }
       array = []; // отчистка старого массива
       num = number + num; // сложили новое число
-
       for (let i of num.toString())
-       array.push(parseInt(i));
+        array.push(parseInt(i));
   }
   else return null;// не подходит по усовию
   return array;// возвращаю массив
