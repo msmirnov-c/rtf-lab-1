2c759184e840e0ede23725fa3c2d4d298045808a
Sokolov Max <48237187+Maxerdan@users.noreply.github.com>
2019-10-09 23:50:42 +0500

    All errors must be fixed(quotes, spaces and no constant condition)

--- index.js
+++ index.js
@@ -1,18 +1,16 @@
 function addNumber(array, number) {
-    let stringOfNumbers = "";
+    let stringOfNumbers = '';
     for(let i = 0; i < array.length; i++) {
-        if(typeof(array[i] == "number" && array[i] === Math.trunc(array[i]) && array[i] >= 0 && array[i] <= 9)) {
+        if(typeof(array[i] === 'number' && array[i] === Math.trunc(array[i]) && array[i] >= 0 && array[i] <= 9)) {
             stringOfNumbers += array[i];
         }
         else {
             return null;
         }
     }
-    if(typeof(number) == "number" && number === Math.trunc(number) && number >= 0) {
+    if(typeof(number) == 'number' && number === Math.trunc(number) && number >= 0) {
         stringOfNumbers = parseInt(stringOfNumbers) + number;
-        return stringOfNumbers
-        .split('')
-        .map(item => parseInt(item));
+        return stringOfNumbers.split('').map(item => parseInt(item));
     }
     else {
         return null;
