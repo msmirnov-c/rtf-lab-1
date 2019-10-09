0bbc36e1797c97ab9db0b023f4055aa2eb98d28e
M1M0G <M1M0G@github.com>
2019-10-10 01:03:11 +0500

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,6 @@
 */
 function addNumber(array, number)
 {
-    //var degree = array.length;
     var num = 0;
     let newArray = [];
     var lengthOfElem = array.every(item => item.length === 1);// Решил использовать флаги для того чтобы лучше разобраться с работой every
@@ -12,10 +11,10 @@ function addNumber(array, number)
     var integer = array.every(item => Number.isInteger(item));
     var arrayIsExist = isNaN(array);// Проверка на существование массива
 
-    if (lengthOfElem == true || typeOfElem == true || arrayIsExist == false || integer == true)
+    if (lengthOfElem == true || typeOfElem == true || arrayIsExist == false || integer == true || number.isInteger == true)
     {
         for(let i = 0; i < array.length; i++)
-            if (array[i].isInteger == false || number.isInteger == false) throw null;
+            if (array[i].isInteger == false ) throw null;
         num = array.join();
         num = Number(num.replace(/,/g, ''));
         num += number;
