066dd0589e5b2c4a57c5d5e6ca77146c5d6c7ebe
Kirill Mineev <akerlayg@gmail.com>
2019-10-11 00:38:15 +0500

    Type checks + number edges

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
  * @param number – число, которое нужно прибавить к числу, полученному из массива
 */
 function addNumber(array, number) {
-    if (!array.every((e) => typeof e === 'number')){
+    if (array.some((e) => typeof e !== 'number' || e < 0 || e > 9) || typeof number !== 'number' || number < 0){
         return null
     }
     return parseInt(array.reduce((arr, e) => arr += e.toString())) + number
