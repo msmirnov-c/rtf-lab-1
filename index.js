c1102e7f02804929dcce62f1d816f224402d091c
Sokolov Max <48237187+Maxerdan@users.noreply.github.com>
2019-10-10 20:57:41 +0500

    test

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function addNumber(array, number) {
     let stringOfNumbers = '';
-    if (array.some(item => (typeof(item) != 'number' || item == Math.trunc(item) || item <= 0 || item >= 9))) {
+    if (array.some(item => ((typeof(item) != 'number') || (item == Math.trunc(item)) || (item < 0) || (item > 9)))) {
         return null;
     } else {
         array.every(item => stringOfNumbers += item);
