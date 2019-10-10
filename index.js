abba8c9ea7c9d7125f9bac5c3a3980d297c15a77
Kirill Mineev <akerlayg@gmail.com>
2019-10-11 00:50:40 +0500

    Check integer

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
  * @param number – число, которое нужно прибавить к числу, полученному из массива
 */
 function addNumber(array, number) {
-    if (array.some((e) => typeof e !== 'number' || e < 0 || e > 9) || typeof number !== 'number' || number < 0){
+    if (array.some((e) => !Number.isInteger(e) || e < 0 || e > 9) || !Number.isInteger(number) || number < 0){
         return null;
     }
     return parseInt(array.reduce((arr, e) => arr += e.toString())) + number;
