b459e65fe6c226836a9cb128b2867f0a24d97496
Komodood <36847935+Komodood@users.noreply.github.com>
2019-10-10 22:42:27 +0500

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
  * @param array – массив элементов. Например, [1, 8, 3]
  * @param number – число, которое нужно прибавить к числу, полученному из массива
 */
-function addNumber(array, number) {
+function addNumber(array, number){
     let NewMass = [];
     let NewNum = 0;
     if(array.some(item => isNaN(item) || item === null || item > 9 || item < 0)){//Проверка на подходящие числа
@@ -10,10 +10,8 @@ function addNumber(array, number) {
     }
     if(array.every(item => Number.isInteger(item)))
     {
-        for(let e in array) 
-            if (array[e] === null || array[e] > 9 || array[e] < 0 ) return null; 
-        NewNum = parseInt(array.join('')) + number;
-        NewMass = NewNum.toString.split('').map(Number); 
+        NewNum = parseInt(array.join()) + number;
+        NewMass = NewNum.split().map(Number); 
         return NewMass;
     }
 }
