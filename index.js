f1602d983adaf0198eadf7452c1c9a22aca5c3fc
M1M0G <48203270+M1M0G@users.noreply.github.com>
2019-10-10 16:57:14 +0500

    Update index.js
    Попытка номер 55 закомпилить тесты ( изменил условия)

--- index.js
+++ index.js
@@ -9,32 +9,23 @@ function addNumber(array, number)
     var lengthOfElem = array.every(item => item.length === 1);// Решил использовать флаги для того чтобы лучше разобраться с работой every
     var typeOfElem = array.every(item => typeof item === 'number');//Проверка на тип данных
     var integer = array.every(item => Number.isInteger(item));
-    //var arrayIsExist = isNaN(array);// Проверка на существование массива
+    var arrayIsExist = isNaN(array);// Проверка на существование массива
 
-    if (lengthOfElem == false || typeOfElem == false || integer == false || number.isInteger == false )
+    if (lengthOfElem === true && typeOfElem === true && arrayIsExist === false && integer === true && number.isInteger === true)
+    {
+        //for(let i = 0; i < array.length; i++)
+        //    if (array[i].isInteger === false ) throw null;
+            //if (array[i].isInteger == false ) throw null;
+        num = array.join();
+        num = Number(num.replace(/,/g, ''));
+        num += number;
+        newArray = num.toString(10).split('').map(Number);
+    }
+    else
         return null;// не подходит по усовию
-    //for(let i = 0; i < array.length; i++)
-    //    if (array[i].isInteger === false) throw null;
-
-    num = array.join();
-    num = Number(num.replace(/,/g, ''));
-    num += number;
-    newArray = num.toString(10).split('').map(Number);
     return newArray;// возвращаю массив
-}
 
 module.exports = {
     addNumber
 };
 
-// прошлый вариант парсинга, до изобретения join();
-//for(let i = 0; i < array.length; i++) // из массива в число
-//  {
-//      if (Math.floor(i) !== i) throw null;
-//      num += (array[i]*Math.pow(10,degree));
-//      degree--;
-//  }
-//  num = number + num; // сложили новое число
-//    if (Math.floor(array[i]) !== i) throw null;
-//for (let i of num.toString(10))
-//  newArray.push(parseInt(i));
