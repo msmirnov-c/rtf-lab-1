6125b6df977154517d0e958ff0eb157d0d6bcdbc
Maria <48256725+mashaartemeva@users.noreply.github.com>
2019-10-10 15:29:11 +0500

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ function addNumber(array, number) {
     let arr = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
     let res = 0;
     let resArr = [];
-     if (array.length == 0 || ! Number.isInteger(number)|| number < 0) return null;
+    if (array.length == 0 || ! Number.isInteger(number)|| number < 0) return null;
     for (let i = 0; i < array.length; i++) {
         if (arr.indexOf(array[i]) == -1 ) return null;
         res += (Math.pow(10, array.length - i - 1) * array[i]);
