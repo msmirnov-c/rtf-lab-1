2c3ee430dcf2e92310d0207e4e744e7d55c80c13
M1M0G <M1M0G@github.com>
2019-10-10 20:01:38 +0500

    Update index.js

--- index.js
+++ index.js
@@ -3,13 +3,30 @@
 * @param number – число, которое нужно прибавить к числу, полученному из массива 
 */ 
 
-function addNumber(array, number) {
-    let stringForAddNumber;
-    if (array.some(elem => isNaN(elem) || elem > 9  || elem < 0 || elem === null || Math.floor(elem) !== elem))
-        return null;
-    stringForAddNumber = parseInt(array.join('')) + number;
-    return stringForAddNumber.toString().split('').map(Number);
-}
+function addNumber(array, number) 
+{ 
+    var num = 0; 
+    let newArray = []; 
+    var typeOfElem = array.every(item => typeof item === 'number');//Проверка на тип данных 
+    var integer = array.every(item => Number.isInteger(item)); 
+    if (typeOfElem === true && integer === true && number > 0 && Number.isInteger(number) === true) 
+    { 
+        for(let i = 0; i < array.length; i++) 
+            if (array[i].isNaN === true || array[i] > 9 || array[i] < 0 ) return null; 
+
+        num = array.join(); 
+        num = Number(num.replace(/,/g, '')); 
+        num += number; 
+        newArray = num.toString(10).split('').map(Number); 
+    } 
+    else 
+        return null;// не подходит по усовию 
+    return newArray;// возвращаю массив 
+} 
+
+
+
+
 module.exports = {
     addNumber
 };
