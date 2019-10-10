f8400b012827a032948c8c9b0929089b060150e5
M1M0G <M1M0G@github.com>
2019-10-10 18:52:55 +0500

    Update index.js

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ function addNumber(array, number)
     var integer = array.every(item => Number.isInteger(item)); 
     //var arrayIsExist = isNaN(array);// Проверка на существование массива 
 
-    if (lengthOfElem === true && typeOfElem === true && integer === true && number > 0) 
+    if (lengthOfElem === true && typeOfElem === true && integer === true && number > 0 && number.isInteger === true) 
     { 
         for(let i = 0; i < array.length; i++) 
             if (array[i].isNaN === false ) throw null; 
@@ -24,6 +24,7 @@ function addNumber(array, number)
         return null;// не подходит по усовию 
     return newArray;// возвращаю массив 
 }
+
 
 module.exports = {
     addNumber
