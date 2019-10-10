32d737fb0a55973ec0da71170782aebddd8f6d1e
Mix2511 <47683500+Mix2511@users.noreply.github.com>
2019-10-10 16:02:20 +0500

    try number 1

--- index.js
+++ index.js
@@ -3,6 +3,16 @@
  * @param number – число, которое нужно прибавить к числу, полученному из массива
 */
 function addNumber(array, number) {
+    if (typeof number === 'number') {
+        var arr;
+        for (var i = 0; i < array.lenght; i++)
+            arr += array(i);
+        var res = parseInt(arr) + number;
+        alert(res);
+    }
+    else {
+        return null;
+    }
     // Решение задачи
 }
 
