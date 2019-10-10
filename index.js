8170457bd66358129eb4cc178ef817eda2f17d28
Ulano4ka <>
2019-10-10 17:51:30 +0500

    first task

--- index.js
+++ index.js
@@ -4,6 +4,22 @@
 */
 function addNumber(array, number) {
     // Решение задачи
+    for (let el of array) {
+        if (!Number.isInteger(el) || el < 0 || el > 9)
+            return null;
+    }
+    let numberStr = array[0] + '';
+    let i = 1;
+    while (i < array.length) {
+        numberStr =  numberStr + array[i];
+        i++;
+    }
+    numberStr = String(Number(numberStr) + number);
+    let result = [];
+    for (let j = 0; j < numberStr.length; j++) {
+        result.push(Number(numberStr[j]));
+    }
+    return result;
 }
 
 module.exports = {
