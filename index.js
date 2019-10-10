98a8be24e7de82278df1d2c270883ad45cf950cd
Komodood <36847935+Komodood@users.noreply.github.com>
2019-10-11 00:07:00 +0500

    Update index.js

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ function addNumber(array, number){
     let NewMass = [];
     let NewNum = '';
     for(var i = 0; i<array.lenght; i++){
-        if(array.some(item => isNaN(item) || item === null || item > 9 || item < 0))    return null;
-        else if(array.every(item => Number.trunc(item) === item && (typeof(item) === 'number')) === true)
+        if(array[i]  = isNaN || array[i]  == null || array[i] > 9 || array[i] < 0)    return null;
+        else if(Math.trunc(array[i]) === array[i] /*&& (typeof(array[i]) === 'number')*/)
             NewNum += array[i];
     }
     NewNum = parseInt(NewNum) + number;
