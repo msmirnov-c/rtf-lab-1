b0cf8803f816951e2c9168a8573a0590487ce44a
Mix2511 <47683500+Mix2511@users.noreply.github.com>
2019-10-10 23:01:51 +0500

    1

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
  * @param array – массив элементов. Например, [1, 8, 3]
  * @param number – число, которое нужно прибавить к числу, полученному из массива
  */
-//(
+//1
 function addNumber(array, number) {
     var str = '';
     for (var i = 0; i < array.lenght; i++) {
