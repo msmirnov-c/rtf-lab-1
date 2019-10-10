3ca46841968d75be8ad18e403554133fd8179645
M1M0G <M1M0G@github.com>
2019-10-10 19:20:29 +0500

    Update index.js

--- index.js
+++ index.js
@@ -6,10 +6,8 @@ function addNumber(array, number)
 { 
     var num = 0; 
     let newArray = []; 
-    //var lengthOfElem = array.every(item => item.toString.length === 1);// Решил использовать флаги для того чтобы лучше разобраться с работой every 
     var typeOfElem = array.every(item => typeof item === 'number');//Проверка на тип данных 
     var integer = array.every(item => Number.isInteger(item)); 
-    //var arrayIsExist = isNaN(array);// Проверка на существование массива 
 
     if (typeOfElem === true && integer === true && number > 0 && Number.isInteger(number) === true) 
     { 
@@ -25,10 +23,7 @@ function addNumber(array, number)
         return null;// не подходит по усовию 
     return newArray;// возвращаю массив 
 } 
-var number = 34; 
-var array = [1]; 
-var n = addNumber(array, number) 
-console.log(n);
+
 module.exports = {
     addNumber
 };
