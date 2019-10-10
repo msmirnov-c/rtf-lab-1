34bd37e84a868a767f06abab0640c3a00fd325c3
Sokolov Max <48237187+Maxerdan@users.noreply.github.com>
2019-10-10 16:42:36 +0500

    Вернул цикл, исправил пробелы после if

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 function addNumber(array, number) {
     let stringOfNumbers = '';
-    if (array.every(item => (typeof(item) === 'number' && item === Math.trunc(item) && item >= 0 && item <= 9)))
-    {
-        array.every(item => stringOfNumbers += item);
-    } else {
-        return null;
-    }
+    for (let i = 0; i < array.length; i++) { 
+        if (typeof(array[i]) === 'number' && array[i] === Math.trunc(array[i]) && array[i] >= 0 && array[i] <= 9) { 
+            stringOfNumbers += array[i]; 
+        } else { 
+            return null; 
+        } 
+    } 
     stringOfNumbers = (parseInt(stringOfNumbers) + number).toString(10);
     return stringOfNumbers.split('').map(item => parseInt(item));
 }
