6a6aeaa07819a8502d8bac914285dedfe77f022a
Vladimir <43116496+VladimirSTAR0911@users.noreply.github.com>
2019-10-10 17:03:40 +0500

    Update index.js

--- index.js
+++ index.js
@@ -3,20 +3,19 @@
  * @param number – число, которое нужно прибавить к числу, полученному из массива
 */
 function addNumber(array, number) {
-    let in_num = "";
+    let in_num = '';
     let array_numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
     array.forEach(function (arr) {
-        if (typeof arr == "number");
-        else { in_num = "exit"; return null; }
+        if (typeof arr == 'number');
+        else { in_num = 'exit'; return null; }
         if (arr == array_numbers[0] || arr == array_numbers[1] || arr == array_numbers[2] ||
             arr == array_numbers[3] || arr == array_numbers[4] || arr == array_numbers[5] ||
             arr == array_numbers[6] || arr == array_numbers[7] || arr == array_numbers[8] ||
-            arr == array_numbers[9]) {
-        }
-        else { in_num = "exit"; return null; }
+            arr == array_numbers[9]);
+        else { in_num = 'exit'; return null; }
         in_num += arr;
     });
-    return in_num == "exit" ? null : Array.from((Number(in_num) + number) + "");
+    return in_num == 'exit' ? null : Array.from((Number(in_num) + number) + '');
 }
 
 module.exports = {
