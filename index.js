5fe5226555b5e6cd9f99f25cb83d1f964be7a7c6
M1M0G <48203270+M1M0G@users.noreply.github.com>
2019-10-10 17:16:28 +0500

    Correct, i know
    изменение условий

--- index.js
+++ index.js
@@ -6,13 +6,16 @@ function addNumber(array, number)
 {
     var num = 0;
     let newArray = [];
-    var lengthOfElem = array.every(item => item.length === 1);// Решил использовать флаги для того чтобы лучше разобраться с работой every
+    var lengthOfElem = array.every(item => item.toString.length === 1);// Решил использовать флаги для того чтобы лучше разобраться с работой every
     var typeOfElem = array.every(item => typeof item === 'number');//Проверка на тип данных
     var integer = array.every(item => Number.isInteger(item));
     var arrayIsExist = isNaN(array);// Проверка на существование массива
 
-    if (lengthOfElem === true && typeOfElem === true && arrayIsExist === false && integer === true && number.isInteger === true)
+    if (lengthOfElem === true && typeOfElem === true && arrayIsExist === true /*integer === true && isInteger(number) === true*/)
     {
+        //for(let i = 0; i < array.length; i++)
+        //    if (array[i].isInteger === false ) throw null;
+            //if (array[i].isInteger == false ) throw null;
         num = array.join();
         num = Number(num.replace(/,/g, ''));
         num += number;
