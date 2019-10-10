f9d0fd63503815d7f326ef62f93a4856ad87b2db
Mix2511 <47683500+Mix2511@users.noreply.github.com>
2019-10-10 22:50:35 +0500

    (

--- index.js
+++ index.js
@@ -1,19 +1,20 @@
 /**
  * @param array – массив элементов. Например, [1, 8, 3]
  * @param number – число, которое нужно прибавить к числу, полученному из массива
-*/
+ */
 function addNumber(array, number) {
-    if (typeof number === 'number') {
-        var arr;
-        for (var i = 0; i < array.lenght; i++)
-            arr += array(i);
-        var res = parseInt(arr) + number;
-        return res;
+    var str = '';
+    for (var i = 0; i < array.lenght; i++) {
+        if (array[i] === Math.trunc(array[i]) && typeof (number) === 'number' && typeof (array[i]) === 'number' && array[i] >= 0 && array[i] <= 9) {
+            str += array[i];
+        }
+        else {
+            return null;
+        }
     }
-    else {
-        return null;
-    }
-    // Решение задачи
+    str = parseInt(str) + number;
+    var result=str.toString(10).split('');
+    return result.map(item => parseInt(item));
 }
 
 module.exports = {
