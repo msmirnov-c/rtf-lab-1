5eac385163b8647e06df931b90ade94777a72a12
M1M0G <M1M0G@github.com>
2019-10-10 19:48:01 +0500

    Прошу не ругаться, я пытаюсь разобраться в коде с помощью других версий
    Я УЖЕ НЕ ЗНАЮ ЧТО НЕ ТАК(

--- index.js
+++ index.js
@@ -2,28 +2,14 @@
 * @param array  – массив элементов. Например, [1, 8, 3] 
 * @param number – число, которое нужно прибавить к числу, полученному из массива 
 */ 
-function addNumber(array, number) 
-{ 
-    var num = 0; 
-    let newArray = []; 
-    var typeOfElem = array.every(item => typeof item === 'number');//Проверка на тип данных 
-    var integer = array.every(item => Number.isInteger(item)); 
-    var isNaNka = array.every(item => isNaN(item));
-    if (typeOfElem === true && integer === true && number > 0 && Number.isInteger(number) === true && isNaNka === true) 
-    { 
-        for(let i = 0; i < array.length; i++) 
-            if (array[i].isNaN === false || array[i] > 9 || array[i] < 0 ) return null; 
 
-        num = array.join(); 
-        num = Number(num.replace(/,/g, '')); 
-        num += number; 
-        newArray = num.toString(10).split('').map(Number); 
-    } 
-    else 
-        return null;// не подходит по усовию 
-    return newArray;// возвращаю массив 
-} 
-
+function addNumber(array, number) {
+    let stringForAddNumber;
+    if (array.some(elem => isNaN(elem) || elem > 9  || elem < 0 || elem === null || Math.floor(elem) !== elem))
+        return null;
+    stringForAddNumber = parseInt(array.join('')) + number;
+    return stringForAddNumber.toString().split('').map(Number);
+}
 module.exports = {
     addNumber
 };
