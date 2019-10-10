0640545286d24c39e93fce7d02a3a8c017346057
Sokolov Max <48237187+Maxerdan@users.noreply.github.com>
2019-10-10 20:46:54 +0500

    Снова убрал цикл, использовал метод массива, в условии заменил every на some

--- index.js
+++ index.js
@@ -1,12 +1,10 @@
 function addNumber(array, number) {
     let stringOfNumbers = '';
-    for (let i = 0; i < array.length; i++) { 
-        if (typeof(array[i]) === 'number' && array[i] === Math.trunc(array[i]) && array[i] >= 0 && array[i] <= 9) { 
-            stringOfNumbers += array[i]; 
-        } else { 
-            return null; 
-        } 
-    } 
+    if (array.some(item => typeof(item) != 'number' || item == Math.trunc(item) || item <= 0 || item >= 9)) {
+        return null;
+    } else {
+        array.every(item => stringOfNumbers += item);
+    }
     stringOfNumbers = (parseInt(stringOfNumbers) + number).toString(10);
     return stringOfNumbers.split('').map(item => parseInt(item));
 }
