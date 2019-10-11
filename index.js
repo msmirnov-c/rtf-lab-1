ff84e321c1a13d2c6a7060241f2d8ee7f8265163
Никита Паренюк <48210690+nekitlnp@users.noreply.github.com>
2019-10-11 20:21:41 +0500

    Сделал лабораторную работу

--- index.js
+++ index.js
@@ -1,11 +1,47 @@
 /**
- * @param array – массив элементов. Например, [1, 8, 3]
- * @param number – число, которое нужно прибавить к числу, полученному из массива
+* @param array – массив элементов. Например, [1, 8, 3]
+* @param number – число, которое нужно прибавить к числу, полученному из массива
 */
 function addNumber(array, number) {
-    // Решение задачи
-}
-
-module.exports = {
-    addNumber
-};
+    if ( array.some(isSomeArrayValueNaN) ) {
+    return null;
+    }
+    
+    if ( !isNumberPositiveAndInteger(number) ) {
+    return;
+    }
+    
+    let firstTerm = '';
+    array.forEach( item => {
+    firstTerm += item.toString();
+    });
+    
+    const sum = parseInt(firstTerm, 10) + number; 
+    const sumStr = sum.toString();
+    let outputArray = [];
+    
+    for (let i in sumStr) {
+    outputArray.push(parseInt(sumStr[i], 10)); 
+    }
+    
+    return outputArray;
+    }
+    
+    function isSomeArrayValueNaN(item) {
+    return isNotANumber(item);
+    }
+    
+    function isNumberPositiveAndInteger(num) {
+    if ( !isNotANumber(num) && isInteger(num) && (num >= 0) ) {
+    return true;
+    }
+    return false;
+    }
+    
+    function isNotANumber(item) {
+    return typeof item !== 'number';
+    }
+    
+    function isInteger(num) {
+    return (num ^ 0) === num;
+    }
