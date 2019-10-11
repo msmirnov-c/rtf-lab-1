d116e63dc9de85bb07187091de23a05e6a57e343
Komodood <36847935+Komodood@users.noreply.github.com>
2019-10-11 18:11:50 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+//console.log(addNumber([5,'qwerfs',7],100))
 /**
  * @param array – массив элементов. Например, [1, 8, 3]
  * @param number – число, которое нужно прибавить к числу, полученному из массива
@@ -5,7 +6,7 @@
 function addNumber(array, number){
     let NewMass = [];
     let NewNum = 0;
-    if(array.some(item => isNaN(item) || item === null || item > 9 || item < 0 || Math.trunc(item) != item || typeof(item) !== 'number')) return null;//Проверка на подходящие числа
+    if(array.some(item => isNaN(item) || item === 'undefined' || item === null || item > 9 || item < 0 || Math.trunc(item) != item || typeof(item) !== 'number' || Number.isInteger(item) === false)) return null;//Проверка на подходящие числа
     NewNum = parseInt(array.join(''));
     NewNum = NewNum + number;
     NewMass = NewNum.toString(10).split().map(Number); 
