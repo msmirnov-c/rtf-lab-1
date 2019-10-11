ea9443fabcbd64c10ada51e3dbccb1c0e29211d5
Сергей Русинов <fftron@me.com>
2019-10-11 16:35:21 +0500

    First task

--- index.js
+++ index.js
@@ -3,7 +3,18 @@
  * @param number – число, которое нужно прибавить к числу, полученному из массива
 */
 function addNumber(array, number) {
-    // Решение задачи
+    if (!Number.isInteger(number) || number < 0 ||
+        !Array.isArray(array) || array.length === 0 || (array.length > 0 && array[0] === 0) ||
+        !array.every((x) => Number.isInteger(x) && x <= 9 && x >= 0)) return null;
+
+    const result = [];
+
+    for (let i = array.length - 1; i >= 0 || number > 0; i--){
+        number += i >= 0 ? array[i] : 0;
+        result.unshift(number % 10);
+        number = number / 10 | 0;
+    }
+    return result;
 }
 
 module.exports = {
