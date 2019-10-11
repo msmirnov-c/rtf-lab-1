b7903b1c5e76f2deec481c890be5fa8a17975cbd
Dmitriy Baygulov <baygulov.d@yandex.ru>
2019-10-11 21:31:51 +0500

    'first_task10'

--- index.js
+++ index.js
@@ -3,38 +3,31 @@
  * @param number – число, которое нужно прибавить к числу, полученному из массива
 */
 function addNumber(array, number) { 
-    let error = false; //флаг ошибки 
-    
-    if (array != null) 
-    { 
-        let stringNumber = ""; //строка в которую положим цифры 
-        let resultNumber = 0; 
+    if (!Array.isArray(array) || !Number.isInteger(number) || number < 0)
+        return null;
 
-        if (array.length !== 0) 
-        { 
-            array.forEach(element => { //идем по массиву и присоединяем цифры к строке 
-                if (element >= 0) // если тип элемента число, то 
-                    stringNumber += element; //присоединяем к строке 
-                if (typeof(element) != "number") 
-                    error = true; //иначе поднимаем флаг ошибки 
-            }); 
-        resultNumber = parseInt(stringNumber); //получаем число из строки 
-        } 
-    
-        resultNumber += number; //прибавляем заданное число 
-        resultNumber = resultNumber.toString(); //возвращаемся к строке 
-    
-        let resultArray = [] //массив результата 
-        for (let i = 0; i < resultNumber.length; i++) //идем по символам строки 
-            resultArray.push(parseInt(resultNumber[i])); //переводим символ строки в число и записываем в массив 
-        if (error == false) 
-            return resultArray; //возвращаем результат 
-    } 
-    else 
-        error = true; 
-        
-    if (error === true) //если флаг ошибки поднят, то 
-        return null; 
+    let stringNumber = '';
+    for (let i = 0; i < array.length; i++)
+    {
+        if (array[i] > 0 && Number.isInteger(array[i]))
+            stringNumber += array[i];
+        else if (typeof(array[i]) === "string")
+            return null;
+    }
+
+    let intNumber = parseInt(stringNumber) + number;
+    let res = intNumber.toString();
+    let arr = [];
+    if (array.length == 0)
+    {
+        arr.push(number);
+        return arr;
+    }
+
+    for (let i = 0; i < res.length; i++)
+        arr.push(parseInt(res[i]));
+
+    return arr;
 }
 
 module.exports = {
