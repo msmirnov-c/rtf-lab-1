8ade35c398e4c060d61e28b47fde39bc32ec48f0
Anna-Skorodskaya <konoplya12318@gmail.com>
2019-10-12 21:41:38 +0500

    meow

--- index.js
+++ index.js
@@ -3,17 +3,21 @@
  * @param number – число, которое нужно прибавить к числу, полученному из массива
 */
 function addNumber(array, number) {
+    if (typeof number != "number") return null;
     var stringFromArray = "";
     var returnedArray = [];
-      array.ForEach(function(item, index, array) {
-          if (item === 0 || item === 1 || item === 2 || item === 3 || item === 4 || item === 5 || item === 6 || item === 7 || item === 8 || item === 9 )
-          stringFromArray += item;
-          else return null;
-      });
-      parseInt(stringFromArray);
-      stringFromArray += number; 
-      returnedArray = split((toString(stringFromArray), ""));
-      return returnedArray;  
+    array.ForEach(function(item, index, array) {
+        if (item === 0 || item === 1 || item === 2 || item === 3 || item === 4 || item === 5 || item === 6 || item === 7 || item === 8 || item === 9 )
+        stringFromArray += item;
+        else return null;
+    });
+    parseInt(stringFromArray);
+    stringFromArray += number;
+    sNumber = stringFromArray.toString();
+    for (var i = 0; i < stringFromArray.length; i++) {
+        returnedArray.push(+sNumber.charAt(i)); 
+    } 
+    return returnedArray;  
 }
 
 module.exports = {
