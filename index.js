b01267fea413701cda2056a51532de67b5d61f6d
Ольга Сосина <56439623+Svet1aya@users.noreply.github.com>
2019-10-12 12:20:45 +0500

    first task

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
  * @param number – число, которое нужно прибавить к числу, полученному из массива
 */
 function addNumber(array, number) {
-    // Решение задачи
+    let x=1;
 }
 
 module.exports = {
