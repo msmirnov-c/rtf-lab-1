6916509f1e37e6d3826d2ab36baa198d059d980d
RexMaleficus <salandrey1999@gmail.com>
2019-10-13 00:35:31 +0500

    Task1
    Хз, как исправлять этот код стайл

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 function addNumber(array, number) {
-    {
         for (let i = 0; i < array.length; i++) {
             if (typeof array[i] == "number") {
 
                 if (array[i] < 0 || array[i] > 9) {
                     return null;
                 }
+
             } else {
                 return null;
             }
@@ -14,13 +14,15 @@ function addNumber(array, number) {
         if (number < 0 || !Number.isInteger(number)) {
             return null;
         }
+
         let arrayNumber = 0;
         for (let i = 0; i < array.length - 1; i++) {
             arrayNumber += array[i];
             arrayNumber *= 10;
         }
+
         arrayNumber += array[array.length - 1];
         arrayNumber += number;
+
         return arrayNumber.toString().split('');
-    }
 }
