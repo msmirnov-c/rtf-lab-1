ac4a5d17e91316dc51697750ea9c3ac4fba02722
Maria <48256725+mashaartemeva@users.noreply.github.com>
2019-10-13 00:46:38 +0500

    Update index.js
    Просто решила сделать код немного лучше

--- index.js
+++ index.js
@@ -3,12 +3,12 @@
  * @param number – число, которое нужно прибавить к числу, полученному из массива
 */
 function addNumber(array, number) {
-    let arr = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
     let res = 0;
-    let resArr = [];
-    if (array.length == 0 || ! Number.isInteger(number)|| number < 0) return null;
+    if (array.length == 0 || !Number.isInteger(number)
+        || !array.every((num) => Number.isInteger(num))
+        || !array.every((num) => num<=9 && num>=0) ||number < 0) return null;
     for (let i = 0; i < array.length; i++) {
-        if (arr.indexOf(array[i]) == -1) return null;
+        
         res += (Math.pow(10, array.length - i - 1) * array[i]);
     }
     res += number;
