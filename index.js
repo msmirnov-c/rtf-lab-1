b6b5db1ec868acfb8e7155c9d9dac07662a985c8
Adnanmada <56203664+Adnanmada@users.noreply.github.com>
2019-10-12 11:15:46 -0700

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,23 @@
  * @param number – число, которое нужно прибавить к числу, полученному из массива
 */
 function addNumber(array, number) {
-    // Решение задачи
+    var inputArray = [] ;
+        var size = 5 ;
+    for (var i=0; i<size; i++) 
+    { inputArray[i] = prompt('enter element ' + (i+1)) ; }
+    console.log(inputArray);
+    var x = inputArray.join('');
+    var integer1 = parselnt(x,10);
+    console.log(integer1);
+    var integer2=prompt('enter the positive number');
+    var integer4= parselnt(integer2,10);
+    var integer3=integer1+integer4;
+    var Arr=integer3.tostring(10.split('').map(number);
+    console.log(Arr);
+    
+    
+    
+    
 }
 
 module.exports = {
