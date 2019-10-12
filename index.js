2dc0ab4ccd52d1f1c430c96aefd7d7240f05c83b
Ruslan <zzombfixr@gmail.com>
2019-10-12 23:52:38 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,3 @@
-/**
- * @param array – массив элементов. Например, [1, 8, 3]
- * @param number – число, которое нужно прибавить к числу, полученному из массива
-*/
 function addNumber(array, number) {
     function check(value, index, array) {
         var result = true;
