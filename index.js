b48bcc5f8389bff166543f8c07ac709eb854c928
Dmitriy Baygulov <baygulov.d@yandex.ru>
2019-10-13 00:11:27 +0500

    444

--- index.js
+++ index.js
@@ -29,7 +29,6 @@ function addNumber(array, number) {
         arr.push(number);
         return arr;
     }
-
     for (let i = 0; i < res.length; i++)
     {
         arr.push(parseInt(res[i], 10));
