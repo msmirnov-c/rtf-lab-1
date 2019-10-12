1c7aa04cfac9c5088c7051fd1addc8b84b48ed10
Dmitriy Baygulov <baygulov.d@yandex.ru>
2019-10-12 23:48:52 +0500

    222

--- index.js
+++ index.js
@@ -4,28 +4,34 @@
 */
 function addNumber(array, number) { 
     if (!Array.isArray(array) || !Number.isInteger(number) || number < 0)
+    {
         return null;
+    }
 
     let stringNumber = '';
     for (let i = 0; i < array.length; i++)
     {
         if (array[i] > 0 && Number.isInteger(array[i]))
+        {
             stringNumber += array[i];
+        }
         else if (typeof(array[i]) === "string")
+        {
             return null;
+        }
     }
 
-    let intNumber = parseInt(stringNumber) + number;
-    let res = intNumber.toString();
-    let arr = [];
-    if (array.length == 0)
+    let intNumber = parseInt(stringNumber, 10) + number;
+    const res = intNumber.toString();
+    const arr = [];
+    if (array.length === 0)
     {
         arr.push(number);
         return arr;
     }
 
     for (let i = 0; i < res.length; i++)
-        arr.push(parseInt(res[i]));
+        arr.push(parseInt(res[i], 10));
 
     return arr;
 }
