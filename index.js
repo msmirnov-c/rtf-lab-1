7a6d5e2baf6c8721b638d1f3f3c04c4c2a777236
Dmitriy Baygulov <baygulov.d@yandex.ru>
2019-10-13 00:50:03 +0500

    777

--- index.js
+++ index.js
@@ -1,11 +1,31 @@
-/**
- * @param array – массив элементов. Например, [1, 8, 3]
- * @param number – число, которое нужно прибавить к числу, полученному из массива
-*/
-function addNumber(array, number) {
-    // Решение задачи
-}
-
-module.exports = {
-    addNumber
-};
+/** 
+* @param array – массив элементов. Например, [1, 8, 3] 
+* @param number – число, которое нужно прибавить к числу, полученному из массива 
+*/ 
+function addNumber(array, number) { 
+    if (!Array.isArray(array) || !Number.isInteger(number) || number < 0) 
+        return null; 
+    
+    let stringNumber = ''; 
+    for (let i = 0; i < array.length; i++) 
+    { 
+    if (array[i] > 0 && Number.isInteger(array[i])) 
+        stringNumber += array[i]; 
+    else if (typeof(array[i]) === "string") 
+        return null; 
+    } 
+    
+    let intNumber = parseInt(stringNumber) + number; 
+    let res = intNumber.toString(); 
+    let arr = []; 
+    if (array.length == 0) 
+    { 
+        arr.push(number); 
+        return arr; 
+    } 
+    
+    for (let i = 0; i < res.length; i++) 
+        arr.push(parseInt(res[i])); 
+    
+    return arr; 
+} 
