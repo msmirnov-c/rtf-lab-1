cb355d0800f30d968a2375d3a1435629a12adbaa
liminestick <56450358+liminestick@users.noreply.github.com>
2019-10-12 13:59:12 +0500

    Update index.js

--- index.js
+++ index.js
@@ -3,12 +3,12 @@
  * @param number – число, которое нужно прибавить к числу, полученному из массива
 */
 function addNumber(array, number) {
-    if(!(array.every(item => typeof item === 'number') && item>0 && item <9))
+    if(!(array.every(item => typeof item === 'number') && item > 0 && item < 9))
     return null
     
-    var CisloMassiva = array.join('');
-    var Sum = Number(CisloMassiva) + number;
-    var Sum = String(Sum);
+    let CisloMassiva = array.join('');
+    let Sum = Number(CisloMassiva) + number;
+    let Sum = String(Sum);
     Sum = Sum.split('');
     Sum = Sum.map(item => Number(item));
     return SUM
