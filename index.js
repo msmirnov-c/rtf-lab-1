287555618416796c0e7c1b03a8253f635c41ac19
TiDebilHAHAHA <karrapyzzz1@mail.ru>
2019-10-12 12:20:19 +0500

    Task1
    Функция первой лабы

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
-/**
- * @param array – массив элементов. Например, [1, 8, 3]
- * @param number – число, которое нужно прибавить к числу, полученному из массива
-*/
-function addNumber(array, number) {
-    // Решение задачи
+﻿function addNumber(array, number){
+    if (number < 0 || !Number.isInteger(number))
+    {
+        return null;
+    }
+    if (!isCorrectArray(array) && !isCorrectNumber(number))
+        return null;
+    let arr = parseInt(array.toString().replace(/,/gm, ''));
+    let result = arr + number;
+    return Array.from(String(result), Number);
 }
 
-module.exports = {
-    addNumber
-};
+function isCorrectArray(array){
+    for(let item of array)
+    {
+        if (Number(item) !== item || item > 9 || item < 0)
+            return false;
+    }
+    return true;
+}
