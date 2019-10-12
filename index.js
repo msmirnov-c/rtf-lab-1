db5300f973c1310d221d858163aa5370edf1e95d
Marina <marina-maksa@mail.ru>
2019-10-12 21:07:22 +0500

    first task

--- index.js
+++ index.js
@@ -3,7 +3,11 @@
  * @param number – число, которое нужно прибавить к числу, полученному из массива
 */
 function addNumber(array, number) {
-    // Решение задачи
+    if (array.every(item => typeof item != 'number'))
+        return null;
+    let arrayNumber = 0;
+    arrayNumber = Number(arrayNumber.join('')) + number;
+    return arrayNumber.split('');
 }
 
 module.exports = {
