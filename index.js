7a90479da4a5eae91bb73fb0fae301a20d3e94df
ma.selyankin <reckleyoff@gmail.com>
2019-10-13 17:54:31 +0500

    Completed Task

--- index.js
+++ index.js
@@ -3,9 +3,20 @@
  * @param number – число, которое нужно прибавить к числу, полученному из массива
 */
 function addNumber(array, number) {
-    // Решение задачи
+    let checkNumbers = array.every(function (i) {
+        return Number.isInteger(i);
+    });
+
+    if (!checkNumbers) return null;
+
+    let num = parseInt(array.join(''));
+    let numberStr = (num + number).toString();
+
+    let arr = [];
+    for (let i = 0; i < numberStr.length; i++)
+        arr[i] = parseInt(numberStr[i]);
+
+    return arr;
 }
 
-module.exports = {
-    addNumber
-};
+module.exports = addNumber;
