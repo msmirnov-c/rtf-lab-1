33f16aa1482f96307c0287a3df455c3660a6d437
D.Kuvashov <d.kuvashov@gmail.com>
2019-10-13 23:24:54 +0500

    first task

--- index.js
+++ index.js
@@ -11,10 +11,11 @@ function addNumber(array, number) {
         for (let j = 1; j < 10; j++) {
             if (array[i] === j) {
                 if (number > 0 && Number.isInteger(number) === true) {
-                    helper++; }
-                else {
-                    return null; } }
-            else {
+                    helper++;
+                } else {
+                    return null;
+                }
+            } else {
                 return null; } } }
     if (helper === array.length) {
         for (let i = 0; i < array.length; i++) {
