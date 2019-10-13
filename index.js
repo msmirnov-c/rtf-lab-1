a44f6eb1d1ae90f7bb29ba3fd71839a8a68c7e31
Olga Korelina <56450175+pvpilizassal@users.noreply.github.com>
2019-10-13 22:25:51 +0500

    'index'

--- index.js
+++ index.js
@@ -1,10 +1,28 @@
-/**
- * @param array – массив элементов. Например, [1, 8, 3]
- * @param number – число, которое нужно прибавить к числу, полученному из массива
-*/
+const array = [1, 2, 3];
+number = 44;
 function addNumber(array, number) {
     // Решение задачи
+    let sum = 0, i;
+    for (i = 0; i < array.length; i++) {
+        if ( typeof array[i]!="string" && array[i]<=9 && array[i]>=0 )
+            sum = sum * 10 + array[i];
+        else
+            return null;
+    }
+
+    sum=sum+number;
+    let sum1 = [];
+    let len = sum.toString().length;
+    for (i =len ; i > 0 ; i--) {
+        sum1[i-1] = sum % 10;
+        sum = Math.trunc(sum/10);
+    }
+
+    return(sum1)
 }
+
+sum = addNumber(array,number);
+console.log(sum);
 
 module.exports = {
     addNumber
