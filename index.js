74a0697370856e9af4ab432f604d53de910e445b
Alexander <AD.Popov.IT@gmail.com>
2019-10-13 20:52:19 +0500

    changed condition (version 2.0)

--- index.js
+++ index.js
@@ -3,16 +3,14 @@
  * @param number – число, которое нужно прибавить к числу, полученному из массива
 */
 function addNumber(array, number) {
-    let secondNumberIsNumber = isNumber(number);
-    let arrayElementsIsNumber = array.every((item) => (isNumber(item)) && (item >= 0) && item <= 9);
-    let amount = Number(array.join('')) + number;
-    let arr = String(amount).split('').map((item) => Number(item));
-
-    return secondNumberIsNumber && arrayElementsIsNumber ? arr : null;
-}
-
-function isNumber(number) {
-    return number !== 'string';
+    const arrayElementsIsNumber = array.every((item) => (typeof(item) === 'number' && (item >= 0) && (item <= 9)));
+    const NumberIsNumber = typeof(number) === 'number';
+    if (arrayElementsIsNumber && NumberIsNumber) {
+        const amount = Number(array.join('')) + number;
+        return String(amount).split('').map((item) => Number(item));
+    } else {
+        return null;
+    }
 }
 
 module.exports = {
