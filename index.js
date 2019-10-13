13895061c0ddae90b2c00b4a3ab4b8dd404a3f2b
Komodood <36847935+Komodood@users.noreply.github.com>
2019-10-13 16:01:13 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,3 @@
-/** 
- * @param array – массив элементов. Например, [1, 8, 3]
- * @param number – число, которое нужно прибавить к числу, полученному из массива
-*/
 function addNumber(array, number) { 
     let NewMass = [];
     let NewNum = 0;
