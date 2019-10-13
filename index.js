3df137f6e50ca26b510f30a9fc94eb0f715762bf
Sokolov Max <48237187+Maxerdan@users.noreply.github.com>
2019-10-13 14:53:45 +0500

    Убрал лишнюю переменную - флаг

--- index.js
+++ index.js
@@ -1,10 +1,5 @@
 function addNumber(array, number) {
-    let failFlag = false;
     if (array.some((item) => typeof (item) !== 'number' || item !== Math.trunc(item) || item < 0 || item > 9)) {
-        failFlag = true;
-    }
-
-    if (failFlag === true) {
         return null;
     }
 
