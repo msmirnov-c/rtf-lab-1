94a228de0746d6226aedebeace213e0d3c4734fa
TiDebilHAHAHA <karrapyzzz1@mail.ru>
2019-10-13 14:06:51 +0500

    Task1
    tryyy

--- index.js
+++ index.js
@@ -4,26 +4,25 @@
         return null;
     }
 
-    if (!isCorrectArray(array)) {
-        return null;
-    }
-
-    let arr = parseInt(array.toString().replace(/,/gm, ''));
-
-    let result = arr + number;
-
-    return Array.from(String(result), Number);
-
-
     function isCorrectArray(array) {
         for (let item of array) {
             if (Number(item) !== item || item > 9 || item < 0) {
                 return false;
             }
         }
-
         return true;
     }
+
+    if (!isCorrectArray(array)) {
+        return null;
+    }
+
+        let arr = parseInt(array.toString().replace(/,/gm, 10));
+
+        let result = arr + number;
+
+        return Array.from(String(result), Number);
+        
 }
 module.exports = {
     addNumber,
