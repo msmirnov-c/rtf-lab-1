9abe318a5ddd1b3240adedb61085853483ad9bd7
Alexander <AD.Popov.IT@gmail.com>
2019-10-13 16:24:34 +0500

    Solution (version 1.0)

--- index.js
+++ index.js
@@ -2,8 +2,12 @@
  * @param array – массив элементов. Например, [1, 8, 3]
  * @param number – число, которое нужно прибавить к числу, полученному из массива
 */
+
 function addNumber(array, number) {
-    // Решение задачи
+    return (number != "string")
+            && array.every(item => typeof(item) == 'number' 
+            && (item >= 0) 
+            && (item <= 9)) ? String(Number(array.join('')) + number).split('') : null;
 }
 
 module.exports = {
