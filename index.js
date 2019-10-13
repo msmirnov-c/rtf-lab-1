8217a8c0fb9f2b2c6166939defc1fc1ca5ff96c2
yashachanges <jaygol99@mail.ru>
2019-10-13 19:02:52 +0500

    lab1
    Сделана 1 лабораторная работа

--- index.js
+++ index.js
@@ -4,8 +4,24 @@
 */
 function addNumber(array, number) {
     // Решение задачи
+if (array.every(elem => Number.isInteger(elem) & elem>=0 & elem<=9))
+{
+  let result = "";
+   array.forEach(elem => result += elem);
+   result = +result;
+   result += number;
+   return result.toString().split("");
 }
 
+else return "null";
+
+}
+
+  let array = [1, 8, 3];
+  let num = 5;
+  console.log(addNumber(array, num));
+
+  
 module.exports = {
     addNumber
 };
