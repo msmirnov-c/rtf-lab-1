7af3e6278e7649e560b93f4839a275c91af7699e
AndrewMarshal <37539223+AndrewMarshal@users.noreply.github.com>
2019-10-13 13:16:35 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 /** 
-* @param array – массив элементов. Например, [1, 8, 3] 
-* @param number – число, которое нужно прибавить к числу, полученному из массива 
+*@param array – массив элементов. Например, [1, 8, 3] 
+*@param number – число, которое нужно прибавить к числу, полученному из массива 
 */
-function addNumber(array, number) 
+function addNumber(array, number)
 {
     let numLine = '';
-    array.forEach(function(_item, i) {
+    array.forEach(function callback(_item, i) {
         if (array[i] === Math.ceil(array[i]) && array[i] >= 0 && array[i] < 10 && (typeof(array[i]) === 'number')) {
             numLine += array[i];
         }
@@ -15,7 +15,6 @@ function addNumber(array, number)
     }); 
     return (number + parseInt(numLine)).toString(10).split('').map(item => parseInt(item));
 }
-
 module.exports = { 
     addNumber 
 };
