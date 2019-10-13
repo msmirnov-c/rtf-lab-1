2afd18f426f94c5a0b63024a5e9c4a1dbb096840
yashachanges <jaygol99@mail.ru>
2019-10-13 19:17:31 +0500

    lab1
    bugfix

--- index.js
+++ index.js
@@ -3,25 +3,24 @@
  * @param number – число, которое нужно прибавить к числу, полученному из массива
 */
 function addNumber(array, number) {
-    // Решение задачи
-if (array.every(elem => Number.isInteger(elem) & elem>=0 & elem<=9))
-{
-  let result = "";
-   array.forEach(elem => result += elem);
-   result = +result;
-   result += number;
-   return result.toString().split("");
+  // Решение задачи
+  if (array.every(elem => Number.isInteger(elem) & elem >= 0 & elem <= 9)) {
+    let result = "";
+    array.forEach(elem => result += elem);
+    result = +result;
+    result += number;
+    return result.toString().split("");
+  }
+
+  else return "null";
+
 }
 
-else return "null";
+// let array = [1, 8, 3];
+// let num = 5;
+// console.log(addNumber(array, num));
 
-}
 
-  let array = [1, 8, 3];
-  let num = 5;
-  console.log(addNumber(array, num));
-
-  
 module.exports = {
-    addNumber
+  addNumber
 };
