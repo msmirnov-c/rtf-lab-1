73eee0b14ade201b7008878f19d7c38ef7181134
liminestick <56450358+liminestick@users.noreply.github.com>
2019-10-14 04:32:36 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 function addNumber(array, number) {
     if (!(array.every(item => typeof item ===  'number' )));
         return null
-        var cislomassiva =  array.join('') 
-        var sum = String((Number(cislomassiva) + number)).split('').map(item => Number(item));
+        let cislomassiva =  array.join('') 
+        let sum = String((Number(cislomassiva) + number)).split('').map(item => Number(item));
     return sum 
-    
-    
-    };
+    }
+
+    addNumber([1, 0, 9], 2); // 109 + 2 = 111; => [1, 1, 1]
+    addNumber([2, 5, 1], 5); // 251 + 5 = 256; => [2, 5, 6]
+    addNumber([1], 4020); // 1 + 4020 = 4021; => [4, 0, 2, 1]
+    addNumber([1, '4'], 1); // некорректный элемент => null
