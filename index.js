ca8cd36175328114b0552a1dd925fd696942cfb0
Nick Kovalkov <NickOzrRu@gmail.com>
2019-10-13 18:19:25 +0500

    Update

--- index.js
+++ index.js
@@ -6,11 +6,12 @@ function addNumber(array, number) {
     let resultNumber = 0;
     for (let i = 0; i < array.length; i++) {
         if (Number.isInteger(array[i]) && array[i] <= 9 && array[i] >= 0) {
-            resultNumber += array[i] * (10 ** array.length - i - 1);}
+            resultNumber += array[i] * (10 ** array.length - i - 1);
+        }
         else return null;
     }
     resultNumber += number;
-    return resultNumber.toString().split('').map(i => Number(i));
+    return resultNumber.toString().split('').map((i) => Number(i));
 }
 
 module.exports = {
