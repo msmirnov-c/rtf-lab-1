f895f629bc0873e0287787b856ff69cdd93d74dc
LuckyLiar <40919210+LuckyLiar@users.noreply.github.com>
2019-10-13 13:58:38 +0500

    Add files via upload

--- index.js
+++ index.js
@@ -4,9 +4,8 @@ function addNumber(array, number) {
 
     for (let i = 0; i < array.length; i++) {
         if (typeof (array[i]) === 'number' && array[i] >= 0 && array[i] <= 9 && array[i] === Math.trunc(array[i])) {
-            transnum += array[i]; }
-
-        else {
+            transnum += array[i];
+        } else {
             return null;
         }
     }
@@ -16,10 +15,12 @@ function addNumber(array, number) {
     for (let i = 0; i < transnum.length; i++) {
         array1[i] = +transnum[i];
     }
-    return array1;
+
+return array1;
 }
 
 addNumber([1, 0, 9], 2); // 109 + 2 = 111; => [1, 1, 1]
 addNumber([2, 5, 1], 5); // 251 + 5 = 256; => [2, 5, 6]
 addNumber([1], 4020); // 1 + 4020 = 4021; => [4, 0, 2, 1]
 addNumber([1, '4'], 1); // некорректный элемент => null
+eslint.enable
