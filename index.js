fe89d6202440f161eec2e7471a7b521538f1fc77
AndrewMarshal <37539223+AndrewMarshal@users.noreply.github.com>
2019-10-13 19:35:16 +0500

    Update index.js

--- index.js
+++ index.js
@@ -5,13 +5,13 @@
 function addNumber(array, number) {
     let numLine = '';
     array.forEach((_item, i) => {
-        if (array[i] === Math.ceil(array[i]) && array[i] >= 0 && array[i] < 10 && (typeof (array[i]) === 'number' && array[i] === null) && (typeof (number) === 'number')) {
-            return null;
+        if (!(array[i] === Math.ceil(array[i]) && array[i] >= 0 && array[i] < 10 && (typeof (array[i]) === 'number' && array[i] === null) && (typeof (number) === 'number'))) {
+            numLine += array[i];
+
+            return numLine.push;
         }
 
-        numLine += array[i];
-
-        return numLine.push;
+        return null;
     });
 
     return (number + parseInt(numLine, 10)).toString(10).split('').map((item) => parseInt(item, 10));
