afcb6d55668391efc9e3f8caa6b2e06c05734266
khabibullinamaria <56475297+khabibullinamaria@users.noreply.github.com>
2019-10-14 00:17:27 +0500

    Update index.js

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ function addNumber(array, number) {
             return null;
         }
     }
-    
+
     const finalArray = [];
     let value = array.join();
     value = Number(value.replace(/,/g, ''));
@@ -17,7 +17,7 @@ function addNumber(array, number) {
     for (let i = 0; i < value.toString().length; i++) {
         finalArray.push(+value.toString().charAt(i));
     }
-    
+ 
     return finalArray;
 }
 
