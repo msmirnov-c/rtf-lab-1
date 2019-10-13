c4e74a54373add5cda8c29a6122f8ecf440eaf38
Elena Fedulova <48327813+needMoreBlood@users.noreply.github.com>
2019-10-13 12:27:02 +0500

    Add files via upload

--- index.js
+++ index.js
@@ -3,26 +3,27 @@
  * @param number – число, которое нужно прибавить к числу, полученному из массива
 */
 
-function addNumber(array, number){    
+function addNumber(array, number) {    
     let len = array.length - 1;
     let num = 0;    
-    if (array.length < 1)return null;
+    if (array.length < 1) return null;
 
     for (let i = 0; i < array.length; i++) {
-        if (typeof(array[i] !=='number' && array[i] > 9 && array[i] < 0))  return null;    
+        if (typeof (array[i] !== 'number' && array[i] > 9 && array[i] < 0)) return null;
     } 
 
     for (let i = array.length; i >= 1; i--) {
         num += Math.pow(10, len);
-        len --;
-    }    
-    let number1 = number + num;    
+        len--;
+    }
+
+    let number1 = number + num;
     const arr = [];
     while (number1>0) {
         arr.unshift(number1 % 10);
         number1= Math.floor(number1 / 10);
     }
-    }
+}
     
     module.exports = {
     
