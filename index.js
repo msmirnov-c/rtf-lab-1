3e52904403d7dde2913891b0ff0b15f572838788
ZoomFix <zzombfixr@gmail.com>
2019-10-13 12:27:44 +0500

    Rab

--- index.js
+++ index.js
@@ -3,7 +3,17 @@
  * @param number – число, которое нужно прибавить к числу, полученному из массива
 */
 function addNumber(array, number) {
-    // Решение задачи
+    function check(value) {
+        let result = true;
+        if (value < 0 || value > 9 || typeof value !== 'number') {
+            result = false;
+        }
+        return result;
+    }
+    if (!array.every(check)) {
+        return null;
+    }
+    return (parseInt(array.join(''), 10) + number).toString().split('').map(Number);
 }
 
 module.exports = {
