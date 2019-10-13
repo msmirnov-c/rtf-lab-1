b74112933f05c3b87e7e65c80a46c8fb53232bae
Elena Fedulova <48327813+needMoreBlood@users.noreply.github.com>
2019-10-13 12:10:50 +0500

    Add files via upload

--- index.js
+++ index.js
@@ -5,28 +5,21 @@
 
 function addNumber(array, number) {    
     let len = array.length - 1;
-    let num = 0;
-    
-    if (array.length < 1)  return null;
-    
-    for (let i = 0; i < array.length; i++)
-    {
+    let num = 0;    
+    if (array.length < 1) return null;    
+    for (let i = 0; i < array.length; i++) {
         if (typeof(array[i] !=='number' && array[i] > 9 && array[i] < 0))  return null;    
-    }
-    
+    }    
     for (let i = array.length; i >= 1; i--){
         num += Math.pow(10, len);
         len --;
-    }
-    
-    let number1 = number + num;
-    
+    }    
+    let number1 = number + num;    
     const arr = [];
     while (number1>0){
         arr.unshift(number1%10);
         number1= Math.floor(number1/10);
     }
-    console.log(array.length)
     }
     
     module.exports = {
