bcac235a71f502fa5dfe3d9b5ecad1fda64d740b
Guzy99 <56511953+Guzy99@users.noreply.github.com>
2019-10-13 23:21:21 +0500

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,7 +8,7 @@ module.exports = {
             { blankLine: 'always', prev: ['if', 'for', 'while', 'switch'], next: '*' },
         ],
         camelcase: ['error'],
-        'linebreak-style': ["error", "unix"],
+        'linebreak-style': 'off',
         'no-plusplus': ['off'],
         'no-console': ['error'],
         'arrow-parens': ['error', 'as-needed']
