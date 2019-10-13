ff87f4d64569bb472704b3ae27aa962604153f78
LuckyLiar <40919210+LuckyLiar@users.noreply.github.com>
2019-10-13 20:55:34 +0500

    Add files via upload

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function addNumber(array, number) {
     const finalArr = [];
-    let translatedNum = ''; 
+    let translatedNum = '';
     for (let i = 0; i < array.length; i++) {
         if (!(typeof (array[i]) === 'number' && array[i] >= 0 && array[i] <= 9 && array[i] === Math.trunc(array[i]))) {
             return null;
