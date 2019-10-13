3c06591ade224d275ae5f7cbb32cbc94c3279700
Anna-Skorodskaya <konoplya12318@gmail.com>
2019-10-13 23:41:15 +0500

    murrr

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,16 +1,22 @@
 module.exports = {
-    extends: 'eslint-config-airbnb-base',
-    rules: {
-        indent: ['error', 4],
-        'padding-line-between-statements': [
-            'error',
-            { blankLine: 'always', prev: '*', next: 'return' },
-            { blankLine: 'always', prev: ['if', 'for', 'while', 'switch'], next: '*' },
-        ],
-        camelcase: ['error'],
-        'linebreak-style': 'off',
-        'no-plusplus': ['off'],
-        'no-console': ['error'],
-        'arrow-parens': ['error', 'as-needed']
+    "env": {
+        "browser": true,
+        "es6": true
+    },
+    "globals": {
+        "Atomics": "readonly",
+        "SharedArrayBuffer": "readonly"
+    },
+    "parserOptions": {
+        "ecmaFeatures": {
+            "jsx": true
+        },
+        "ecmaVersion": 2018,
+        "sourceType": "module"
+    },
+    "plugins": [
+        "react"
+    ],
+    "rules": {
     }
 };
--- index.js
+++ index.js
@@ -3,23 +3,21 @@
  * @param number – число, которое нужно прибавить к числу, полученному из массива
 */
 function addNumber(array, number) {
-    if (typeof number !== "number") return null;
-    let stringFromArray = "";
-    let returnedArray = [];
-    array.ForEach(function(item) {
-        if (item === 0 || item === 1 || item === 2 || item === 3 
-            || item === 4 || item === 5 || item === 6 || item === 7 
+    if (typeof number !== 'number') return null;
+    let stringFromArray = '';
+    const returnedArray = [];
+    array.ForEach( function(item) {
+        if (item === 0 || item === 1 || item === 2 || item === 3
+            || item === 4 || item === 5 || item === 6 || item === 7
             || item === 8 || item === 9 )
-        {
             stringFromArray += toString(item);
-        }
         else return null;
     });
     const finalNumber = parseInt(stringFromArray)+number;
-    for (var i = 0; i < toString(finalNumber).length; i++) {
-        returnedArray.push(+finalNumber.charAt(i)); 
+    for (let i = 0; i < toString(finalNumber).length; i++) {
+        returnedArray.push( + finalNumber.charAt(i));
     } 
-    return returnedArray;  
+        return returnedArray;  
 }
 
 module.exports = {
