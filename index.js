76af7ad43173e827bb0b854754d2026243016fc2
Alexander <AD.Popov.IT@gmail.com>
2019-10-13 16:42:03 +0500

    Fixed style errors (version 1.1)

--- index.js
+++ index.js
@@ -4,12 +4,12 @@
 */
 
 function addNumber(array, number) {
-    return (number != "string")
-            && array.every(item => typeof(item) == 'number' 
-            && (item >= 0) 
-            && (item <= 9)) ? String(Number(array.join('')) + number).split('') : null;
+    return (number !== 'string')
+            && array.every(item => (typeof (item) === 'number'
+            && (item >= 0)
+            && (item <= 9))) ? String(Number(array.join('')) + number).split('') : null;
 }
 
 module.exports = {
-    addNumber
+    addNumber,
 };
