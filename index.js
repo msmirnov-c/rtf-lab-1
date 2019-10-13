23f4bf6d245b6f3884e99b7f73732b17d383065d
Marina <marina-maksa@mail.ru>
2019-10-13 12:50:17 +0500

    try3

--- index.js
+++ index.js
@@ -3,8 +3,9 @@
  * @param number – число, которое нужно прибавить к числу, полученному из массива
 */
 function addNumber(array, number) {
-    if (array.every(item => typeof item != 'number'))
+    if (array.every(item => typeof item !== 'number')){
         return null;
+    }
     let arrayNumber = 0;
     arrayNumber = Number(array.join('')) + number;
     return arrayNumber.toString().split('');
