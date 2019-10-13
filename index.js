8f19ba10a70112e287f394fb6934fa170917673e
khabibullinamaria <56475297+khabibullinamaria@users.noreply.github.com>
2019-10-13 23:48:11 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+/* eslint linebreak-style: ["error", "windows"] */
 /**
  * @param array – массив элементов. Например, [1, 8, 3]
  * @param number – число, которое нужно прибавить к числу, полученному из массива
