b15a32bb367a5f5ccc06d145e7efb765c77a8167
TiDebilHAHAHA <karrapyzzz1@mail.ru>
2019-10-13 15:26:27 +0500

    Task1
    Laba

--- index.js
+++ index.js
@@ -3,9 +3,23 @@
  * @param number – число, которое нужно прибавить к числу, полученному из массива
 */
 function addNumber(array, number) {
-    // Решение задачи
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] < 0 || array[i] > 9 || parseInt(array[i], 10) === array[i]) {
+            return null;
+        }
+
+        if (number < 0 || !Number.isInteger(number)) {
+            return null;
+        }
+
+        let arr = parseInt(array.toString().replace(/,/gm, ''), 10);
+
+        let result = arr + number;
+
+        return Array.from(String(result), Number);
+    }
 }
 
 module.exports = {
-    addNumber
+    addNumber,
 };
