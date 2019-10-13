b5910b425e4d4893b1cc6ab986ffc978b9ad432f
RexMaleficus <salandrey1999@gmail.com>
2019-10-13 11:51:54 +0500

    Task1
    Очередной код стайл

--- index.js
+++ index.js
@@ -2,12 +2,14 @@
  * @param array – массив элементов. Например, [1, 8, 3]
  * @param number – число, которое нужно прибавить к числу, полученному из массива
  */
-function addNumber(array, number){
+function addNumber(array, number) {
 // Решение задачи
 
     for (let i = 0; i < array.length; i++) {
+
         if (typeof array[i] === 'number') {
-            if(array[i] < 0 || array[i] > 9) {
+
+            if (array[i] < 0 || array[i] > 9) {
                 return null;
             }
         } else {
@@ -20,12 +22,13 @@ function addNumber(array, number){
     }
 
     let arrayNumber = 0;
-    for(let i = 0; i < array.length - 1; i++) {
+
+    for (let i = 0; i < array.length - 1; i++) {
         arrayNumber += array[i];
         arrayNumber *= 10;
     }
     arrayNumber += array[array.length - 1];
-    
+
     arrayNumber += number;
 
     return arrayNumber.toString().split('');
