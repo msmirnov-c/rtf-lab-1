9c565953b50661ae434dfcae6cac342a065dddd3
Marina <marina-maksa@mail.ru>
2019-10-13 13:52:53 +0500

    Bags are fixed

--- task1_base.spec.js
+++ task1_base.spec.js
@@ -2,7 +2,7 @@ const {addNumber} = require('./index');
 
 const cases = [
     { array: [1, 2, 4], number: 1, expected: [1, 2, 5] },
-    { array: [null], number: 125, expected: null }
+    { array: [null], number: 125, expected: null },
 ];
 
 describe('add number', function () {
