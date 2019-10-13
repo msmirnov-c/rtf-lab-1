d3db2d0c8efe9764dee74175a307d504c5dbfa24
TiDebilHAHAHA <karrapyzzz1@mail.ru>
2019-10-13 07:04:35 +0500

    Task1
    Реализованная функция из 1 лабы

--- index.js
+++ index.js
@@ -1,9 +1,27 @@
-/**
+﻿/**
  * @param array – массив элементов. Например, [1, 8, 3]
  * @param number – число, которое нужно прибавить к числу, полученному из массива
 */
 function addNumber(array, number) {
     // Решение задачи
+    if (number < 0 || !Number.isInteger(number))
+    {
+        return null;
+    }
+    if (!isCorrectArray(array))
+        return null;
+    let arr = parseInt(array.toString().replace(/,/gm, ''));
+    let result = arr + number;
+    return Array.from(String(result), Number);
+}
+
+function isCorrectArray(array){
+    for(let item of array)
+    {
+        if (Number(item) !== item || item > 9 || item < 0)
+            return false;
+    }
+    return true;
 }
 
 module.exports = {
