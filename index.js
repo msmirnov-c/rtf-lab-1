9e67e4de6c6f88a5ee806e7f1dc6d7cc63888658
TiDebilHAHAHA <karrapyzzz1@mail.ru>
2019-10-13 12:02:51 +0500

    Task1
    try

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-﻿/**
+﻿﻿/**
  * @param array – массив элементов. Например, [1, 8, 3]
  * @param number – число, которое нужно прибавить к числу, полученному из массива
-*/
+ */
 function addNumber(array, number) {
     // Решение задачи
     if (number < 0 || !Number.isInteger(number))
