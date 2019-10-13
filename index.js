a28eb3841570689167fe7e9cf1aa67444c76ffe2
AndrewMarshal <37539223+AndrewMarshal@users.noreply.github.com>
2019-10-13 13:22:04 +0500

    Update index.js

--- index.js
+++ index.js
@@ -2,8 +2,7 @@
 *@param array – массив элементов. Например, [1, 8, 3] 
 *@param number – число, которое нужно прибавить к числу, полученному из массива 
 */
-function addNumber(array, number)
-{
+function addNumber(array, number) {
     let numLine = '';
     array.forEach(function callback(_item, i) {
         if (array[i] === Math.ceil(array[i]) && array[i] >= 0 && array[i] < 10 && (typeof(array[i]) === 'number')) {
