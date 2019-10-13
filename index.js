98f6ca542ba65fcdbbdf66c47d2eeafa6372f685
LuckyLiar <40919210+LuckyLiar@users.noreply.github.com>
2019-10-13 13:22:17 +0500

    Add files via upload

--- index.js
+++ index.js
@@ -2,9 +2,10 @@ function addNumber(array, number) {
     const array1 = [];
     let transnum = '';
 
-    for(let i = 0; i < array.length; i++) {
-        
-        if(typeof(array[i]) === 'number' && array[i] >= 0 && array[i] <= 9 && array[i] === Math.trunc(array[i])) {
+    for (let i = 0; i < array.length; i++)
+    {
+
+        if (typeof (array[i]) === 'number' && array[i] >= 0 && array[i] <= 9 && array[i] === Math.trunc(array[i])) {
             transnum += array[i];
         }
 
@@ -13,12 +14,12 @@ function addNumber(array, number) {
         }
     }
 
-    transnum = (parseInt(transnum) + number).toString(10);
+    transnum = (+transnum + number).toString(10);
 
-    for(let i = 0; i < transnum.length; i++) {
-        array1[i] = parseInt(transnum[i]);
+    for (let i = 0; i < transnum.length; i++) {
+        array1[i] = +transnum[i];
     }
-    
+  
     return array1;
 }
 
