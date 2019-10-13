ff7ff7f3d662873b0cb191c381491d8a67abb850
LEXA-E6ATb <votencevaleksejj@gmail.com>
2019-10-14 04:52:24 +0500

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
  * @param number – число, которое нужно прибавить к числу, полученному из массива
 */
 function addNumber(array, number) {
-    let stringArray;
+    let stringArray = '';
     for (let i = 0; i < array.lenght; i++) {
         if (typeof (array[i]) === 'number' && array[i] >= 0 && array[i] <= 9) {
             stringArray += array[i].toString();
