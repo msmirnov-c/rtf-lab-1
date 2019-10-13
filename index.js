2f853b3bfc9cd77337325f04b454ee5adcf224ab
Денис Ткачук <liptonb@ya.ru>
2019-10-13 20:23:07 +0500

    Исправил ошибки eslint

--- index.js
+++ index.js
@@ -1,19 +1,23 @@
-/*eslint linebreak-style: ["error", "windows"]*/
+/* eslint linebreak-style: ["error", "windows"] */
 /**
  * @param array – массив элементов. Например, [1, 8, 3]
  * @param number – число, которое нужно прибавить к числу, полученному из массива
-*/
+ */
 function addNumber(array, number) {
-    let megaNumber ='';
-    for(let i of array){
-        if (typeof i === 'number') {
-            megaNumber += i;}
+    let megaNumber = '';
+    for (let i = 0; i < array.length; i++) {
+        if (typeof array[i] === 'number') {
+            megaNumber += i;
+        }
         else {
-            return null;}}
+            return null;
+        }
+    }
     const strMegaNumber = '' + (parseInt(megaNumber, 10) + number);
     const numberArray = strMegaNumber.split('');
     return numberArray.map(function foo(e) {
-        return parseInt(e, 10);});
+        return parseInt(e, 10);
+    });
 }
 
 module.exports = {
