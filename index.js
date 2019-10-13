66b5e7333d47ddb7f1dba05c2d85a8d4078b25f9
Daniil Popov <p.danya@mail.ru>
2019-10-13 22:46:01 +0500

    fix-all-comment

--- index.js
+++ index.js
@@ -3,36 +3,23 @@
  * @param number – число, которое нужно прибавить к числу, полученному из массива
 */
 function addNumber(array, number) {
-    let origStr = '';
-    const resultArr = [];
-    let resultNumber;
-    let resultStr = '';
-    let fail = false;
-
-    if (array.length === 0) {
-        resultArr.push(number);
-
+    const checkArr = array.every(item => typeof item === 'number');
+    if(checkArr){
+        let resultArr = [];
+        if (array.length === 0) {
+            resultArr.push(number);
+            return resultArr;
+        }
+        
+        let origNum = parseInt(array.join(''), 10);
+        origNum += number;
+        const numStr = origNum.toString()  
+        resultArr = numStr.split('');
+        resultArr = resultArr.map(numStr => parseInt(numStr));
+        console.log(numStr, typeof numStr); 
         return resultArr;
     }
-
-    array.forEach((item) => {
-        if (typeof (item) === 'number') origStr += item;
-        else fail = true;
-    });
-
-    if (fail === true) {
-        return null;
-    }
-
-    resultNumber = parseInt(origStr, 10);
-    resultNumber += number;
-    resultStr = String(resultNumber);
-
-    for (let i = 0; i < resultStr.length; i++) {
-        resultArr.push(parseInt(resultStr[i], 10));
-    }
-
-    return resultArr;
+    return null;
 }
 
 module.exports = {
