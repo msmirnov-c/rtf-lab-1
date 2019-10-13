32c8f50441e3d343193c0463bb9947c324d223e8
Денис Ткачук <liptonb@ya.ru>
2019-10-13 20:07:09 +0500

    Стилистически изменил if, унифицировал парсинг в инт, применил map, добавил больше новых переменных.

--- index.js
+++ index.js
@@ -1,23 +1,19 @@
+/*eslint linebreak-style: ["error", "windows"]*/
 /**
  * @param array – массив элементов. Например, [1, 8, 3]
  * @param number – число, которое нужно прибавить к числу, полученному из массива
 */
 function addNumber(array, number) {
     let megaNumber ='';
-    for(let i of array)
-    {
-        if(typeof(i) === 'number' && !isNaN(i)) {
-            megaNumber += i;
-        }
+    for(let i of array){
+        if (typeof i === 'number') {
+            megaNumber += i;}
         else {
-            return null;
-        }
-    }
-    let strMegaNumber = '' + ( parseInt(megaNumber) + number);
-    let numberArray = strMegaNumber.split('');
-    return numberArray.map(function(e) {
-        return parseInt(e);
-    });
+            return null;}}
+    const strMegaNumber = '' + (parseInt(megaNumber, 10) + number);
+    const numberArray = strMegaNumber.split('');
+    return numberArray.map(function foo(e) {
+        return parseInt(e, 10);});
 }
 
 module.exports = {
