744f9d82ac64d80867b139e35d46e3d6a800147e
TiDebilHAHAHA <karrapyzzz1@mail.ru>
2019-10-13 17:19:04 +0500

    Task1
    FASfsafas

--- index.js
+++ index.js
@@ -2,20 +2,22 @@
  * @param array – массив элементов. Например, [1, 8, 3]
  * @param number – число, которое нужно прибавить к числу, полученному из массива
  */
-function addNumber(array, number) {
-    if (number < 0 || !Number.isInteger(number)) {
+function addNumber(array, number){
+    if (number < 0  !Number.isInteger(number)){
         return null;
     }
 
-    for (const item of array) {
-        if (Number(item) !== item || item > 9 || item < 0) {
-            return null;
-        }
-
-        const arr = parseInt(array.toString().replace(/,/gm, ''), 10);
-        const result = arr + number;
-        return Array.from(String(result), Number);
+    for (let i = 0; i < array.length; i++){
+        if (typeof array[i] !== 'number'  array[i] > 9 || array[i] < 0)
+        return null;
     }
+    const arr = parseInt(
+        array
+            .toString()
+            .replace(/,/gm, '')
+    );
+    const result = arr + number;
+    return Array.from(String(result), Number);
 }
 module.exports = {
     addNumber,
