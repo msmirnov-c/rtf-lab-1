2163abb5e30387148d2726583eb7b51df06d6b9d
Денис Ткачук <liptonb@ya.ru>
2019-10-13 19:44:15 +0500

    Стилистически изменил if, унифицировал парсинг в инт, применил map, добавил больше новых переменных.

--- index.js
+++ index.js
@@ -3,7 +3,21 @@
  * @param number – число, которое нужно прибавить к числу, полученному из массива
 */
 function addNumber(array, number) {
-    // Решение задачи
+    let megaNumber ='';
+    for(let i of array)
+    {
+        if(typeof(i) === 'number' && !isNaN(i)) {
+            megaNumber += i;
+        }
+        else {
+            return null;
+        }
+    }
+    let strMegaNumber = '' + ( parseInt(megaNumber) + number);
+    let numberArray = strMegaNumber.split('');
+    return numberArray.map(function(e) {
+        return parseInt(e);
+    });
 }
 
 module.exports = {
