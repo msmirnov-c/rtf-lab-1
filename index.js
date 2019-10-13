bc3fcf678c86ca4aff369c4396d64bff8b2fa83a
Ruslan <zzombfixr@gmail.com>
2019-10-13 13:13:15 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 function addNumber(array, number) {
     function check(value) {
         if (value < 0 || value > 9 || typeof value !== 'number') { return false; }
-        
+		
         return true;
     }
     if (!array.every(check)) { return null; }
-    
+	
     return (parseInt(array.join(''), 10) + number).toString().split('').map(Number);
 }
 
