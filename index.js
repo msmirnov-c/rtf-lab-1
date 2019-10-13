8a831cd55e7efe9181227a20cb6aa3f72884cdef
AndrewMarshal <37539223+AndrewMarshal@users.noreply.github.com>
2019-10-13 13:07:25 +0500

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@
 function addNumber(array, number) 
 {
     let numLine = '';
-    array.forEach(function addNumber(_item, i) {
+    array.forEach(function(_item, i) {
         if (array[i] === Math.ceil(array[i]) && array[i] >= 0 && array[i] < 10 && (typeof(array[i]) === 'number')) {
             numLine += array[i];
         }
