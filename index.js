88b4babd901605df9d5056d7ea2e1661a27d46ac
TiDebilHAHAHA <karrapyzzz1@mail.ru>
2019-10-13 12:47:54 +0500

    Task1
    try hard bl

--- index.js
+++ index.js
@@ -7,21 +7,26 @@ function addNumber(array, number) {
     if (number < 0 || !Number.isInteger(number)) {
         return null;
     }
-    if (!isCorrectArray(array))
-        return null;
-    let arr = parseInt(array.toString().replace(/,/gm, ''));
-    let result = arr + number;
-    return Array.from(String(result), Number);
-}
 
-function isCorrectArray(array) {
-    for (let item of array) {
-        if (Number(item) !== item || item > 9 || item < 0)
-            return false;
+    if (!isCorrectArray(array)) {
+        return null;
+
+        let a = parseInt(array.toString().replace(/,/gm, ''));
+
+        let result = arrr + number;
+
+        return Array.from(String(result), Number);
     }
-    return true;
-}
+
+    function isCorrectArray(array) {
+        for (let item of array) {
+            if (Number(item) !== item || item > 9 || item < 0) {
+                return false;
+            }
+        }
+        return true;
+    }
 
 module.exports = {
-    addNumber
+        addNumber,
 };
