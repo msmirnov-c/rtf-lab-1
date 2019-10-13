bf80f638bc64a3c6c6ae967c57e92231a6bbdb70
liminestick <56450358+liminestick@users.noreply.github.com>
2019-10-14 04:00:20 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 function addNumber(array, number) {
     if (!((array.every(item => typeof item ===  'number' ) && array.item > 0 && array.item < 9)));
         return null
-        let cislomassiva =  array.join(''); 
-        let sum =  Number(CisloMassiva) + number;
-        let sum =  String(sum).split('').map(item => Number(item));
+        let cislomassiva =  array.join('') 
+        Number(cislomassiva) + number; 
+        let sum = String((Number(cislomassiva) + number)).split('').map(item => Number(item));
     return Sum 
     
     module.exports = {
