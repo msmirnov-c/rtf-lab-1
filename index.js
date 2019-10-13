3266b2082eb95edb26f3fb18a7505471ae7fc298
Yok34 <48248922+Yok34@users.noreply.github.com>
2019-10-13 13:04:21 +0500

    first task

--- index.js
+++ index.js
@@ -4,6 +4,17 @@
 */
 function addNumber(array, number) {
     // Решение задачи
+    if(array.every(item => Number.isInteger(item)))
+    {
+        let str = array.join('');
+        //console.log(typeof (str));
+        let sum = +str + number + '';
+        //console.log(typeof (sum));
+        let a = sum.split('');
+        let b = a.map(item => parseInt(item));
+        return b;
+    }else
+        return null;
 }
 
 module.exports = {
