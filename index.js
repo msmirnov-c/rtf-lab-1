42e4f84909cfcc0b3a6af997bc960f23cbb104bc
Komodood <36847935+Komodood@users.noreply.github.com>
2019-10-13 16:08:11 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-function addNumber(array, number) { 
+function addNumber(array, number) {
     let NewMass = [];
     let NewNum = 0;
-    if (array.some(item => isNaN(item) || item === 'undefined' || item === null || item > 9 || item < 0 || Math.trunc(item) !== item || typeof (item) !== 'number' || Number.isInteger(item) === false)) return null;// Проверка на подходящие числа
-    NewNum = parseInt(array.join(''));
-    NewNum = NewNum + number;
-    NewMass = NewNum.toString(10).split().map(Number); 
+    if (array.some((item) => item ===isNaN(item) || item === 'undefined' || item === null || item > 9 || item < 0 || Math.trunc(item) !== item || typeof (item) !== 'number' || Number.isInteger(item) === false)) return null;// Проверка на подходящие числа
+    NewNum = parseInt(array.join(''), 10);
+    NewNum += number;
+    NewMass = NewNum.toString(10).split().map(Number);
     return NewMass;
 }
 module.exports = {
