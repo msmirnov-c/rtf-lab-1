34b4966a127c7d35dd144a93bc089230463a2bba
Alexander <AD.Popov.IT@gmail.com>
2019-10-13 20:57:10 +0500

    change style (version 2.1)

--- index.js
+++ index.js
@@ -3,14 +3,15 @@
  * @param number – число, которое нужно прибавить к числу, полученному из массива
 */
 function addNumber(array, number) {
-    const arrayElementsIsNumber = array.every((item) => (typeof(item) === 'number' && (item >= 0) && (item <= 9)));
-    const NumberIsNumber = typeof(number) === 'number';
+    const arrayElementsIsNumber = array.every((item) => (typeof item === 'number' && (item >= 0) && (item <= 9)));
+    const NumberIsNumber = typeof (number) === 'number';
     if (arrayElementsIsNumber && NumberIsNumber) {
         const amount = Number(array.join('')) + number;
+       
         return String(amount).split('').map((item) => Number(item));
-    } else {
-        return null;
     }
+    
+    return null;
 }
 
 module.exports = {
