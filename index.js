6f2d800eb46d5acef3a2480f646e146bc95a1bd3
Sokolov Max <48237187+Maxerdan@users.noreply.github.com>
2019-10-13 11:33:50 +0500

    dot

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 function addNumber(array, number) {
-    if (array.some((item) => typeof(item) !== 'number' || item !== Math.trunc(item) || item < 0 || item > 9)) {
-        return null;
+    let failFlag = false;
+    if (array.some((item) => typeof (item) !== 'number' || item !== Math.trunc(item) || item < 0 || item > 9)) {
+        failFlag = true;
     } else {
         let StringOfNumbers = number;
         StringOfNumbers += parseInt(array.reduce((total, current) => total + current.toString()));
-        return StringOfNumbers.toString(10).split('').map(item => parseInt(item));
     }
+    if(failFlag === true) {
+        return null;
+    }
+    return StringOfNumbers.toString(10).split('').map(item => parseInt(item));
 }
 module.exports = {
-    addNumber
+    addNumber,
 };
