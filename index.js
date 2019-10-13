9c3e808a11fb35170d6a83f8a79d33294632fb55
Guzy99 <56511953+Guzy99@users.noreply.github.com>
2019-10-14 00:32:17 +0500

    last version

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
 /**
  * @param array – массив элементов. Например, [1, 8, 3]
  * @param number – число, которое нужно прибавить к числу, полученному из массива
-*/
+ */
 function addNumber(array, number) {
-    for (const value of array) {
-        if (value > 9) return null;
+    for (let i = 0; i < array.length; i++) {
+        if (!Number.isInteger(array[i])) {
+            return null;
+        }
     }
 
-    return Number(array.reduce((sum, current) => sum + current.toString())) + number;
+    const summ = Number(array.reduce((sum, current) => sum + current.toString())) + number;
+
+    return Array.from(summ.toString(), value => parseInt(value, 10));
 }
 
 module.exports = {
