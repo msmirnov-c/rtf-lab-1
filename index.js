e75fa88495bfaa2faa507dbf8ceb5ceb263e9e2d
LuckyLiar <40919210+LuckyLiar@users.noreply.github.com>
2019-10-13 14:06:09 +0500

    Add files via upload

--- index.js
+++ index.js
@@ -18,8 +18,3 @@ function addNumber(array, number) {
 
     return array1;
 }
-
-addNumber([1, 0, 9], 2); // 109 + 2 = 111; => [1, 1, 1]
-addNumber([2, 5, 1], 5); // 251 + 5 = 256; => [2, 5, 6]
-addNumber([1], 4020); // 1 + 4020 = 4021; => [4, 0, 2, 1]
-addNumber([1, '4'], 1); // некорректный элемент => null
