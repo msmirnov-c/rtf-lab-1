942e4e764974c72c6075372bf29cffd994d4ad94
Daniil Popov <p.danya@mail.ru>
2019-10-14 17:47:39 +0500

    fixed-bash-14-10

--- index.js
+++ index.js
@@ -3,16 +3,14 @@
  * @param number – число, которое нужно прибавить к числу, полученному из массива
 */
 function addNumber(array, number) {
-    const allElementsAreNumbers = array.every(item => typeof item === 'number');
+    const allElementsAreNumbers = array.every((item) => typeof (item) === 'number');
     if (allElementsAreNumbers && array.length !== 0) {
         const origNum = parseInt(array.join(''), 10) + number;
         const numStr = origNum.toString();
         const stringArr = numStr.split('');
-        const resultArr = stringArr.map(item => parseInt((item), 10));
-
+        const resultArr = stringArr.map((item) => parseInt((item), 10));
         return resultArr;
     }
-
     return null;
 }
 
