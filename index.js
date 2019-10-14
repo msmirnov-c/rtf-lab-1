c24a62b1c007173f9187d571c1f76e18fe2fcd08
RomanGrishin <56441172+RomanGrishin@users.noreply.github.com>
2019-10-14 21:55:19 +0500

    Update index.js

--- index.js
+++ index.js
@@ -3,17 +3,20 @@
  * @param number – число, которое нужно прибавить к числу, полученному из массива
 */
 function addNumber(array, number) {
-for(let i = 0; i < array.lenght; i++ ) {
-if(typeof array[i] !=='number')
-     {
-         
-      return null;} } 
+    for (let i = 0; i < array.lenght; i++ ) {
+        if(typeof array[i] !=='number') {
+            return null;
+        } 
+    } 
+
    const str = array.join('');
    const strValue = parseInt(str,10);
    const str = strstrValue + number;
-   array = array.concat(strValue.splite(''));
+   array1 = array.concat(strValue.splite(''));
    
-   return array;}
+   return array1;
+}
+
  module.exports = {
       addNumber
 };
