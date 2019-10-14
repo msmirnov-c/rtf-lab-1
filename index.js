161a0dc987558f6993a0df4d97f0d6c96d9aeed5
borodulina19 <48767699+borodulina19@users.noreply.github.com>
2019-10-14 16:21:32 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 function checkArray(array) {
-    return array.some(item => item > 9 || item < 0 || !Number.isInteger(item));
+    return array.some(item => {item > 9 || item < 0 || !Number.isInteger(item)});
 }
 
 function addNumber(array, number) {
     let str = '';
     if (!checkArray(array)) {
-        array.forEach((item) => str += item);
+        array.forEach((item) => {str += item});
         str = (Number(str) + number).toString();
         const newArray = str.split('').map(Number);
 
