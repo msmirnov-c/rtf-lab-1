9ad881d159fe690db476bd56c30df07b2a5b5691
Plifac <plifacplifac@gmail.com>
2019-10-14 21:50:02 +0500

    lab1
    solution

--- index.js
+++ index.js
@@ -3,7 +3,18 @@
  * @param number – число, которое нужно прибавить к числу, полученному из массива
 */
 function addNumber(array, number) {
-    // Решение задачи
+    for (let i = 0; i < array.length; i++) {
+        if (!Number.isInteger(number) && array[i] < 0 && array[i] > 9 && typeof array[i] != 'number') {
+            return null;
+        }
+    }
+
+    let string = array.join('');
+    let summ = Number(string);
+    summ += number;
+    let result = summ.toString().split('');
+
+    return result.map(Number);
 }
 
 module.exports = {
