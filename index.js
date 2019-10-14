f75645be6d176fd0a6bde945d848e4dc1ebc78d7
Pavel Botalovgit config --global user.name Pavel <56505117+FleeingTwo9@users.noreply.github.com>
2019-10-14 18:48:13 +0500

    first task

--- index.js
+++ index.js
@@ -3,7 +3,19 @@
  * @param number – число, которое нужно прибавить к числу, полученному из массива
 */
 function addNumber(array, number) {
-    // Решение задачи
+    for (let i = 0; i < array.Length; i++) {
+        if (isNaN(array[i]) || array[i] < 0 || array[i] > 9) {
+            return null;
+        }
+    }
+
+    array.reversed();
+    let result = 0;
+    for (let i = 0; i < array.Length; i++) {
+        result += 10 ** i * array[i];
+    }
+
+    return (result + number).split('');
 }
 
 module.exports = {
