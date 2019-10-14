ba3e43b3844fc6972c86994819bd19f45ca5aea7
Plifac <plifacplifac@gmail.com>
2019-10-14 15:31:03 +0500

    Update index.js
    w

--- index.js
+++ index.js
@@ -3,13 +3,11 @@
  * @param number – число, которое нужно прибавить к числу, полученному из массива
 */
 function addNumber(array, number) {
-
-
     if (array.every(number >= 0 && number <= 9 && typeof number === 'number')) {
         if (number >= 0 && Number.isInteger((number))) {
-            let string = array.join(' ');
+            let string = array.join('');
             string += number;
-            return string.split(' ');
+            return string.split('');
         }
         else return null;
     }
