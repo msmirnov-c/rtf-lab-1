2bcc5ae0f65fc84b6fac79a4ec33d7cb9534dc6d
lex1688 <a_golubiev@bk.ru>
2019-10-14 23:48:23 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,21 +1,14 @@
 function addNumber(array, number) {
-    var n1 = 0;
+    let n1 = 0;
 
-    if (!(number >= 0 && typeof (number) === 'number') || number !== Math.round()(number)) {
-        return null;
-    }
-
-    if (!array.every(function checkNumber (item){
+    if (!(number >= 0 && typeof (number) === 'number') || number !== Math.round()(number)) return null;
+    
+    if (!array.every(function checkNumber(item) {
         if (item >= 0 && item <= 9 && typeof (item) === 'number') {
-            n1 = n1 * 10 + item;
-
+            n1 = n1 * 10 + item; 
             return true;
-        }
-        
-        else
-            return false;       
-    }))
-        return null;
+        } else return false;       
+    })) return null;
 
     n1 += number;
     return String(n1).split('').map(Number);
