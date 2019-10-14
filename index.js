3ecf61eeb21c4852f67edeba590859af23a9ab1b
Anna-Skorodskaya <konoplya12318@gmail.com>
2019-10-14 23:13:11 +0500

    shthbhbjnjbkhguhygt

--- index.js
+++ index.js
@@ -6,13 +6,14 @@ function addNumber(array, number) {
     let stringFromArray = 0;
     if (array.every((item) => item === 0 || item === 1 || item === 2 || item === 3
         || item === 4 || item === 5 || item === 6 || item === 7
-        || item === 8 || item === 9)){
+        || item === 8 || item === 9)) {
         stringFromArray = array.join('');
-        }
+    }
 
     else return null;
 
     if (typeof number !== 'number') return null;
+
     const finalNumber = parseInt(stringFromArray, 10) + number;
     const returnedArray = finalNumber.toString().split('').map(Number);
 
