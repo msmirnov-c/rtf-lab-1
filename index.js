8ba829431f1ed3918f99f05c4224d1d4cf7aecde
Plifac <plifacplifac@gmail.com>
2019-10-14 18:25:18 +0500

    Update index.js
    1

--- index.js
+++ index.js
@@ -5,7 +5,7 @@
 function addNumber(array, number) {
     let newNumber = 0;
     if (array.every(number >= 0 && number <= 9 && typeof number === 'number')) {
-        
+
         if (number >= 0 && Number.isInteger((number))) {
             const string = array.join('');
             newNumber = Number(string);
