5be2cc28fcea9d1799321354bd3ad26f716a6b7a
Olga Korelina <56450175+pvpilizassal@users.noreply.github.com>
2019-10-14 18:09:49 +0500

    Update index.js

--- index.js
+++ index.js
@@ -2,29 +2,28 @@ const array = [1, 2, 3];
 number = 44;
 
 function addNumber(array, number) {
-    let sum = 0, i;
+    let i;
     for (i = 0; i < array.length; i++) {
-        if (typeof array[i] != "string" && array[i] <= 9 && array[i] >= 0)
+        if (typeof array[i] !== "string" && array[i] <= 9 && array[i] >= 0){
             sum = sum * 10 + array[i];
-        else
-            return null;
+        else return null;
     }
 
     sum = sum + number;
-    let sum1 = [];
-    let len = sum.toString().length;
+    const sum1 = [];
+    const len = sum.toString().length;
     for (i = len; i > 0; i--) {
         sum1[i - 1] = sum % 10;
         sum = Math.trunc(sum / 10);
     }
 
-    return (sum1)
+    return (sum1);
 }
 
-sum = addNumber(array, number);
+const sum = addNumber(array, number);
 console.log(sum);
 
 module.exports = {
     addNumber,
-}
+};
 Enter;
