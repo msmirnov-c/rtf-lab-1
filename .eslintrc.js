26636544904043044d7b8b2f12534736add6ed45
Anna-Skorodskaya <konoplya12318@gmail.com>
2019-10-14 18:00:48 +0500

    murrrr

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,22 +1,19 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es6": true
-    },
-    "globals": {
-        "Atomics": "readonly",
-        "SharedArrayBuffer": "readonly"
-    },
-    "parserOptions": {
-        "ecmaFeatures": {
-            "jsx": true
-        },
-        "ecmaVersion": 2018,
-        "sourceType": "module"
-    },
-    "plugins": [
-        "react"
-    ],
-    "rules": {
-    }
+  env: {
+    browser: true,
+    es6: true,
+  },
+  extends: [
+    'airbnb-base',
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  rules: {
+  },
 };
--- index.js
+++ index.js
@@ -3,24 +3,24 @@
  * @param number – число, которое нужно прибавить к числу, полученному из массива
 */
 function addNumber(array, number) {
-    if (typeof number !== 'number') 
-        return null;
-    let stringFromArray = '';
-    const returnedArray = [];
-    array.ForEach((item)=>{
-        if (item === 0 || item === 1 || item === 2 || item === 3
+  if (typeof number !== 'number') return null;
+  let stringFromArray = '';
+  const returnedArray = [];
+  array.ForEach((item) => {
+    if (item === 0 || item === 1 || item === 2 || item === 3
             || item === 4 || item === 5 || item === 6 || item === 7
-            || item === 8 || item === 9)
-            stringFromArray += toString(item);
-        else return null;
-    })
-    const finalNumber = parseInt(stringFromArray)+number;
-    for (let i = 0; i < toString(finalNumber).length; i++) {
-        returnedArray.push(+finalNumber.charAt(i));
-    } 
-        return returnedArray;  
+            || item === 8 || item === 9) {
+      stringFromArray += toString(item);
+      return stringFromArray;
+    } return null;
+  });
+  const finalNumber = parseInt(stringFromArray, 10) + number;
+  for (let i = 0; i < toString(finalNumber).length; i += 1) {
+    returnedArray.push(+finalNumber.charAt(i));
+  }
+  return returnedArray;
 }
 
 module.exports = {
-    addNumber,
+  addNumber,
 };
