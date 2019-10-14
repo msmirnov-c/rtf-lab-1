dbfb3ba31d060df6f76167e29f3e970899fa7ce6
NorilskUshakova <48198755+NorilskUshakova@users.noreply.github.com>
2019-10-14 10:52:40 +0500

    Update index.js

--- index.js
+++ index.js
@@ -2,8 +2,28 @@
  * @param array – массив элементов. Например, [1, 8, 3]
  * @param number – число, которое нужно прибавить к числу, полученному из массива
 */
-function addNumber(array, number) {
-    // Решение задачи
+function addNumber(array, number) 
+{ 
+	let n;
+	let sum	=0;
+	for (let i of array)
+	{
+		if (typeof(i)!=='number')
+		{
+			return null;
+			break;
+		}
+		else
+		{
+			n=(array.reduce((total, current) => total + current))+number;
+			while(n > 0)
+			{
+  				sum += n % 10;
+  				num = Math.floor(n / 10);
+			}
+			return sum;
+		}
+	}
 }
 
 module.exports = {
