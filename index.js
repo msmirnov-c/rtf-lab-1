d3dbded2d88e264916a6fc3f249e26a4b889f25e
vlasovagalya <galenka_vlasova@mail.ru>
2019-10-14 21:09:47 +0500

    c

--- index.js
+++ index.js
@@ -4,8 +4,19 @@
 */
 function addNumber(array, number) {
     // Решение задачи
+    let numberString = '';
+
+    if (array.some((item) => (Number.isInteger(item) && item >= 0 && item < 10))){ 
+        numberString = array.join('');
+    } else {
+        return null;
+    }
+    
+    numberString = (parseInt(numberString, 10) + number).toString();
+
+    return numberString.split('').map((item) => parseInt(item, 10));
 }
 
 module.exports = {
-    addNumber
+    addNumber,
 };
