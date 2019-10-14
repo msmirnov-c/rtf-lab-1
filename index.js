874f6eb6a7117621010e401410e20cdbfd8b23de
borodulina19 <48767699+borodulina19@users.noreply.github.com>
2019-10-14 17:05:41 +0500

    Fix

--- index.js
+++ index.js
@@ -1,22 +1,19 @@
-function checkArray(array) {
-    function check(element) {
-        return element > 9 || element < 0 || !Number.isInteger(element);
-    }
-
-    return array.some(check);
+function checkArrayElements(array) {
+    
+    return array.some(element => element > 9 || element < 0 || !Number.isInteger(element));
 }
 
 function addNumber(array, number) {
-    let str = '';
-    if (!checkArray(array)) {
-        array.forEach((item) => { str += item; });
-        str = (Number(str) + number).toString();
-        const newArray = str.split('').map(Number);
-
-        return newArray;
+    if (checkArrayElements(array)) {
+        return null;
     }
+    let str = arr.reduce(function(sum, current) {
+        return sum + current;
+    }, '');
+    str = (Number(str) + number).toString();
+    const newArray = str.split('').map(Number);
 
-    return null;
+     return newArray;
 }
 
 module.exports = {
