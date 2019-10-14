0e8ca8d1f7fd4b4924266252848e4f74f8c928da
LuckyLiar <40919210+LuckyLiar@users.noreply.github.com>
2019-10-14 18:29:05 +0500

    Add files via upload

--- index.js
+++ index.js
@@ -1,16 +1,14 @@
 function addNumber(array, number) {
-    let isFromZeroToNine = array.every(item => item >= 0) && array.every(item => item < 10);
-    if (!(array.every(item => Number.isInteger(item))) || !(isFromZeroToNine)) {
+    const isFromZeroToNine = array.every((item) => item >= 0) && array.every((item) => item < 10);
+    if (!(array.every((item) => Number.isInteger(item))) || !(isFromZeroToNine)) {
         return null;
     }
 
     const translatedArr = array.reduce((result, item) => result + item.toString());
     const summedNum = (parseInt(translatedArr, 10) + number).toString();
-    const finalArr = summedNum.split('').map(item => parseInt(item, 10));
+    const finalArr = summedNum.split('').map((item) => parseInt(item, 10));
     return finalArr
 }
-
-console.log(addNumber([11, 4], 1));
 
 module.exports = {
     addNumber,
