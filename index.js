ac242b2d34e1a165e382ae5e0aade3ee8e0d9798
Plifac <plifacplifac@gmail.com>
2019-10-14 18:28:07 +0500

    Update index.js
    2

--- index.js
+++ index.js
@@ -5,7 +5,6 @@
 function addNumber(array, number) {
     let newNumber = 0;
     if (array.every(number >= 0 && number <= 9 && typeof number === 'number')) {
-
         if (number >= 0 && Number.isInteger((number))) {
             const string = array.join('');
             newNumber = Number(string);
@@ -13,7 +12,6 @@ function addNumber(array, number) {
 
             return newNumber.toString.split('').map(Number);
         }
-
     }
 
     return null;
