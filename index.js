8f54caca40ad0c48bd837cc0579737694730d55c
Komodood <36847935+Komodood@users.noreply.github.com>
2019-10-14 16:15:26 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 function addNumber(array, number) {
-    let newMass = [];
     let newNum = 0;
     if (array.some((item) => item > 9 || item < 0 || Math.trunc(item) !== item
     || !Number.isInteger(item))) return null;
