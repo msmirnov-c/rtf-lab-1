c341f3493686762447e5a335a2056d6d79305e66
Komodood <36847935+Komodood@users.noreply.github.com>
2019-10-14 15:54:15 +0500

    Update index.js
    По просьбе вашего кодстайла, разрешено не больше 100 символов на строку, делаю из 1 строки 2 строки.

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 function addNumber(array, number) {
     let newMass = [];
     let newNum = 0;
-    if (array.some((item) => item > 9 || item < 0 || Math.trunc(item) !== item || !Number.isInteger(item))) return null;// Проверка на подходящие числа
+    if (array.some((item) => item > 9 || item < 0 || Math.trunc(item) !== item ||
+    !Number.isInteger(item))) return null;
 
     newNum = parseInt(array.join(''), 10);
     newMass = (newNum + number).toString(10).split('').map(Number);
