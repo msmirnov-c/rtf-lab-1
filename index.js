4ce7bc3b668ae79fefdd9c64338df8997ea45ddc
kozhezhopa <vladikkozh@gmail.com>
2019-10-14 15:59:22 +0500

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ function addNumber(array, number) {
     array.forEach((element) => {
         if (typeof (element) !== 'number') return null;
         else if ((element.toString()).length === 1) { return element };
-        else return null;
+        return null;
     });
     let finalNumber;
     for (let i = 0; i < array.length; i++){
