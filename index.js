687eb9924ca7666337126e227082f2be7f2d3c75
slavker99 <48331491+slavker99@users.noreply.github.com>
2019-10-14 18:09:16 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,12 @@
-/**
- * @param array – массив элементов. Например, [1, 8, 3]
- * @param number – число, которое нужно прибавить к числу, полученному из массива
-*/
 function addNumber(array, number) 
 {
     var f = false;
     var str = '';
-    arr.forEach(function(item, i, array) {
-        str = str+String(item);
-        if ((typeof item) != 'number')
+    for (var i=0;i<array.length;i++) {
+        str=str+String(array[i]);
+        if ((typeof array[i]) != 'number')
             f = true;
-    });
+    }
     var num = String(Number(str) + number);
     var retArr = num.split('',array.length+number);
     for (let i = 0; i < retArr.length; i++)
