81edfb1067cb3f665c84710531ce5939e7f4700b
alenaaleyn1kova <48382053+alenaaleyn1kova@users.noreply.github.com>
2019-10-14 18:47:26 +0500

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,16 @@
  * @param number – число, которое нужно прибавить к числу, полученному из массива
 */
 function addNumber(array, number) {
-    // Решение задачи
+    function right(element) {
+        return element < 9 && element > 0 && Number.isInteger(element);
+    };
+    if (!array.every(right)) {
+        return null;
+    }
+    let sumDigit = Number(array.join('')) + number;
+    let sumArray = sumDigit.toString().split('').map(Number);
+
+    return sumArray;
 }
 
 module.exports = {
