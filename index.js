6b6ab8b574f1f3c459d7a564d04b0c6bcad5b1eb
Komodood <36847935+Komodood@users.noreply.github.com>
2019-10-14 15:58:01 +0500

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ function addNumber(array, number) {
     let newMass = [];
     let newNum = 0;
     if (array.some((item) => item > 9 || item < 0 || Math.trunc(item) !== item
-    ||!Number.isInteger(item))) return null;
+    || !Number.isInteger(item))) return null;
 
     newNum = parseInt(array.join(''), 10);
     newMass = (newNum + number).toString(10).split('').map(Number);
