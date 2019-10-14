16f1b7b6e68e3bd2f33aa6e3265427f9b34ce23e
Дмитрий Шмаргунов <ShmargunovD@gmail.com>
2019-10-14 19:10:05 +0500

    fix 1.0

--- index.js
+++ index.js
@@ -4,20 +4,20 @@
 */
 function addNumber(array, number) {
     let temp;
-    if (!Number.isInteger(number) || number < 0){
+    if (!Number.isInteger(number) || number < 0) {
         return null;
     }
-	
-    array.forEach(function(element){
-        if (!Number.isInteger(element) || element < 0 || element > 9){
+    array.forEach(function(element) {
+        if (!Number.isInteger(element) || element < 0 || element > 9) {
             return null;
         }
         temp += element;
     });
     temp = parseInt(temp) + number;
+
     return temp.toString().split('').map(Number);
 }
 
 module.exports = {
-    addNumber
+    addNumber,
 };
