c764903e008a7a8cc9c9532bacda5b2e9def3931
RomanGrishin <56441172+RomanGrishin@users.noreply.github.com>
2019-10-15 00:08:27 +0500

    Update index.js

--- index.js
+++ index.js
@@ -9,14 +9,13 @@ function addNumber(array, number) {
         }
     }
 
-     const str = array.join('');
-     const strValue = parseInt(str, 10);
-     const sum = strValue + number;
-     const strSum = sum.toSting();
-     const door = strSum.split('');
+    const str = array.join('');
+    const strValue = parseInt(str, 10);
+    const sum = strValue + number;
+    const strSum = sum.toSting();
+    const door = strSum.split('');
      
      return array.concat(door);
-
 }
 
 module.exports = { addNumber };
