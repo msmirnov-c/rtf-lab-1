e9ee7a78480c4ebd3f1ce6e6288491804afedd45
Kirill Lovskoy <777kirill999@mail.ru>
2019-10-14 17:45:44 +0500

    first task2

--- index.js
+++ index.js
@@ -3,25 +3,25 @@
  * @param number – число, которое нужно прибавить к числу, полученному из массива
  */
 function addNumber(array, number) {
-    for (let i = 0; i < array.length; i++) {
-        if (array[i] < 0 || array[i] > 9 || typeof array[i] !== 'number') {
-            return null;
-        }
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] < 0 || array[i] > 9 || typeof array[i] !== "number") {
+      return null;
     }
+  }
 
-    const stringNumber = array.join('');
-    const numberFromString = parseInt(stringNumber, 10);
-    const newNumber = numberFromString + number;
-    const str = newNumber.toString();
-    const arr = str.split('');
+  const stringNumber = array.join("");
+  const numberFromString = parseInt(stringNumber, 10);
+  const newNumber = numberFromString + number;
+  const str = newNumber.toString();
+  const arr = str.split("");
 
-    for (let i = 0; i < arr.length; i++) {
-        arr[i] = parseInt(arr[i], 10);
-    }
+  for (let i = 0; i < arr.length; i++) {
+    arr[i] = parseInt(arr[i], 10);
+  }
 
-    return arr;
+  return arr;
 }
 
 module.exports = {
-    addNumber,
+  addNumber
 };
