13c7a0a84697ca6923f764fab9391f8bbf26cfb2
88ORLOV <44868289+88ORLOV@users.noreply.github.com>
2019-10-14 20:31:44 +0500

    first task

--- index.js
+++ index.js
@@ -3,7 +3,15 @@
  * @param number – число, которое нужно прибавить к числу, полученному из массива
 */
 function addNumber(array, number) {
-    // Решение задачи
+    if (Array.isArray(array) && Number.isInteger(number) && number > 0 && array.every(item => item >= 0 && item < 10 && Number.isInteger(item))){
+      let myString = array.join(""); //преобразуем массив в строку
+      let mySum = Number(myString) + number; //переводим строку в число и складываем
+      let intToStr = String(mySum); //преобразуем число в строку
+      let myResult = intToStr.split("").map(Number); //переводим строку в массив строк и преобразуем в массив чисел
+      return myResult;
+    } else {
+      return null;
+    }
 }
 
 module.exports = {
