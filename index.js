14fa36766e8db4002fdc6a18ce09918373459eee
slavker99 <48331491+slavker99@users.noreply.github.com>
2019-10-14 18:35:20 +0500

    Update index.js

--- index.js
+++ index.js
@@ -2,16 +2,18 @@ function addNumber(array, number) {
     let f = false;
     let str = '';
     for (let i = 0; i < array.length; i++) {
-        str =+ String(array[i]);
-        if ((typeof array[i]) != 'number') {
+        str = + String(array[i]);
+        if ((typeof array[i]) !== 'number') {
             f = true;
         }
     }
-    let num = String(Number(str) + number);
-    let retArr = num.split('',array.length+number);
+
+    const num = String(Number(str) + number);
+    const retArr = num.split('', array.length+number);
     for (let i = 0; i < retArr.length; i++) {
         retArr[i] = Number(retArr[i]);
     }
+
     if ((number < 0) || (f)) {
         return null;
     }
