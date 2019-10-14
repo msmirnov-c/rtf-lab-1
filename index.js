6bc4654d682948a9866ec48cee5820a14fcaa8b1
Alinalalalona <>
2019-10-14 21:49:50 +0500

    Update index.js

--- index.js
+++ index.js
@@ -2,10 +2,10 @@
  * @param array – массив элементов. Например, [1, 8, 3]
  * @param number – число, которое нужно прибавить к числу, полученному из массива
  */
+
 function addNumber(array, number) {
-    if (array.some((element) => (!Number.isInteger(element) || element < 0 || element > 9))) {
+    if (array.some(element => (!Number.isInteger(element) || element < 0 || element > 9)))
         return null;
-    }
 
     let myString = array.join('');
     myString = String(Number(myString) + number);
