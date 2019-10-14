be186d1bd02712288312a74112af1560a9785b49
Alexander <AD.Popov.IT@gmail.com>
2019-10-14 16:41:25 +0500

    Refactoring (version 2.4)

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 /**
  * @param array – массив элементов. Например, [1, 8, 3]
  * @param number – число, которое нужно прибавить к числу, полученному из массива
-*/
+ */
 function addNumber(array, number) {
-    const arrayElementsIsNumber = array.every((item) => (typeof item === 'number' && (item >= 0) && (item <= 9)));
-    const NumberIsNumber = typeof (number) === 'number';
-    if (arrayElementsIsNumber && NumberIsNumber) {
+    const isArrayElementsNumber = array.every((item) => (typeof item === 'number') && (item >= 0) && (item <= 9));
+    const IsSecondParameterNumber = typeof (number) === 'number';
+    if (isArrayElementsNumber && IsSecondParameterNumber) {
         const amount = Number(array.join('')) + number;
 
         return String(amount).split('').map((item) => Number(item));
