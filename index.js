847f500e625b83d3263c0bac431389e3ff7d27ab
farid2509 <farid.dadoboev@gmail.com>
2019-10-14 12:39:11 +0500

    Update index

--- index.js
+++ index.js
@@ -1,11 +1,28 @@
-/**
- * @param array – массив элементов. Например, [1, 8, 3]
- * @param number – число, которое нужно прибавить к числу, полученному из массива
-*/
+// eslint-disable-next-line no-unused-vars
+function digitFind(array, number) {
+    let i = 1;
+    let digit = 0;
+    for(let j = array.length - 1; j >= 0; j--)
+    {
+        if(array[j] > 9 || array[j] < 0 || !Number.isInteger(array[j]))
+        {
+            return null;
+        }
+        digit += array[j] * i;
+        i *= 10;
+    }
+    return digit + number;
+}
+
 function addNumber(array, number) {
-    // Решение задачи
+    let digit = digitFind(array, number);
+    if(digit == null)
+    {
+        return null;
+    }
+    return digit.toString().split('').map(Number);
 }
 
 module.exports = {
-    addNumber
+    addNumber,
 };
