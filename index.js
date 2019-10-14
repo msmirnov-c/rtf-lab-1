68e0c21ffc8504fc38edd0dc03b8eedd01bcac69
88ORLOV <44868289+88ORLOV@users.noreply.github.com>
2019-10-14 20:36:51 +0500

    first task

--- index.js
+++ index.js
@@ -4,10 +4,10 @@
 */
 function addNumber(array, number) {
     if (Array.isArray(array) && Number.isInteger(number) && number > 0 && array.every(item => item >= 0 && item < 10 && Number.isInteger(item))){
-      let myString = array.join(""); //преобразуем массив в строку
-      let mySum = Number(myString) + number; //переводим строку в число и складываем
-      let intToStr = String(mySum); //преобразуем число в строку
-      let myResult = intToStr.split("").map(Number); //переводим строку в массив строк и преобразуем в массив чисел
+      let myString = array.join("");
+      let mySum = Number(myString) + number;
+      let intToStr = String(mySum);
+      let myResult = intToStr.split("").map(Number);
       return myResult;
     } else {
       return null;
