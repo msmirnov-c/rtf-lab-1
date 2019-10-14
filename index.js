bdafd80e4b09ffc1792ba754c5f2b74c8674daf5
M1M0G <M1M0G@github.com>
2019-10-14 15:36:34 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
 function addNumber(array, number) {
     let num = 0;
     let newArray = [];
-    let flag = array.every((item) => Number.isInteger(item));
-    if (flag === true && array.every((item) => item > 0 && item < 9 && item !== null )) {
+    const flag = array.every((item) => Number.isInteger(item));
+    if (flag === true && array.every((item) => item > 0 && item < 9 && item !== null)) {
         num = array.join();
         num = Number(num.replace(/,/g, ''));
         num += number;
         newArray = num.toString(10).split('').map(Number);
     } else return null;
-    return newArray;}
+    
+    return newArray;
+}
 module.exports = {
     addNumber,
 };
