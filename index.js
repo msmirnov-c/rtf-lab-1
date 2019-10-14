8867c231882f7424cdc4e6d2b3098da8f3746ed3
RomanGrishin <56441172+RomanGrishin@users.noreply.github.com>
2019-10-14 19:46:17 +0500

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,6 @@
  * @param number – число, которое нужно прибавить к числу, полученному из массива
 */
 function addNumber(array, number) {
-  if( arr.every(x=>x=>'number'))
   {
  Number(array.reduce((sum, current) => sum + current.toString())) + number;
   return null;}
