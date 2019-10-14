b86aed79a82b16884a21870096f2cf8162c2b464
Anna-Skorodskaya <konoplya12318@gmail.com>
2019-10-14 23:02:25 +0500

    murrrmeow

--- index.js
+++ index.js
@@ -3,25 +3,15 @@
  * @param number – число, которое нужно прибавить к числу, полученному из массива
 */
 function addNumber(array, number) {
+    let stringFromArray = 0;
+    if (array.every((item) => item === 0 || item === 1 || item === 2 || item === 3
+        || item === 4 || item === 5 || item === 6 || item === 7
+        || item === 8 || item === 9))
+        stringFromArray = array.join('');
+    else return null;
     if (typeof number !== 'number') return null;
-
-    let stringFromArray = '';
-    const returnedArray = [];
-    array.ForEach((item) => {
-        if (item === 0 || item === 1 || item === 2 || item === 3
-            || item === 4 || item === 5 || item === 6 || item === 7
-            || item === 8 || item === 9) {
-            stringFromArray += toString(item);
-
-            return stringFromArray;
-        }
-
-        return null;
-    });
     const finalNumber = parseInt(stringFromArray, 10) + number;
-    for (let i = 0; i < toString(finalNumber).length; i += 1) {
-        returnedArray.push(+finalNumber.charAt(i));
-    }
+    const returnedArray = finalNumber.toString().split('').map(Number);
 
     return returnedArray;
 }
