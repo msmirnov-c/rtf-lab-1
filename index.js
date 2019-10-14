d0f7da1bd0e7e5a8a3190bacea03a7f89446a69e
Sangin-Khidirov <48213526+Sangin-Khidirov@users.noreply.github.com>
2019-10-14 16:40:20 +0500

    Update index.js

--- index.js
+++ index.js
@@ -5,22 +5,23 @@
 function addNumber(array, number) {
     let pow = 0;
     let sum = 0;
-    let funcArray = function(item) {
-        return (item < 0 || item > 9 || typeof item !== "number")
-    }
+    const funcArray = function validation(item) {
+        return (item < 0 || item > 9 || typeof item !== 'number');
+    };
     if (array.some(funcArray)) {
         return null;
     }
-    array = array.reverse();
-    array.forEach(function(item) {
-        sum += item * Math.pow(10, pow);
+
+    let answer = array.reverse();
+    answer.forEach(function sumItems(item) {
+        sum += item * 10 ** pow;
         pow++;
     });
-    array = (number + sum).toString().split('');
+    answer = (number + sum).toString().split('');
     
     return array.map(Number);
 }
 
 module.exports = {
-    addNumber
+    addNumber,
 };
