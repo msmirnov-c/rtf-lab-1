33438922434ca38f0649f79aa889d400844f59df
M1M0G <M1M0G@github.com>
2019-10-14 15:44:33 +0500

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ function addNumber(array, number) {
     let num = 0;
     let newArray = [];
     const flag = array.every((item) => Number.isInteger(item));
-    if (flag === true && array.every((item) => item > 0 && item < 9 && item !== null)) {
+    if (flag === true && array.every((item) => item >= 0 && item < 10 && item !== null)) {
         num = array.join();
         num = Number(num.replace(/,/g, ''));
         num += number;
