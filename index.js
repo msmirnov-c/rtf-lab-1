b8d4472527785e38b32066b8b9851936f6a22298
farid2509 <48219711+farid2509@users.noreply.github.com>
2019-10-14 12:55:20 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,23 +1,22 @@
-// eslint-disable-next-line no-unused-vars
 function digitFind(array, number) {
     let i = 1;
     let digit = 0;
-    for(let j = array.length - 1; j >= 0; j--)
-    {
-        if(array[j] > 9 || array[j] < 0 || !Number.isInteger(array[j]))
-        {
+    for (let j = array.length - 1; j >= 0; j--) {
+        if (array[j] > 9 || array[j] < 0 || !Number.isInteger(array[j])) {
             return null;
         }
+
         digit += array[j] * i;
         i *= 10;
     }
+
     return digit + number;
 }
 
 function addNumber(array, number) {
-    let digit = digitFind(array, number);
-    if(digit == null)
-    {
+    const digit = digitFind(array, number);
+    if (digit == null) {
+
         return null;
     }
     return digit.toString().split('').map(Number);
