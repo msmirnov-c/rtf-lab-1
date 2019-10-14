6a2e5aef9699cdb9bd06bf5402d077fcb66bc343
Plifac <plifacplifac@gmail.com>
2019-10-14 15:20:46 +0500

    Update index.js
    i did smth again

--- index.js
+++ index.js
@@ -3,11 +3,9 @@
  * @param number – число, которое нужно прибавить к числу, полученному из массива
 */
 function addNumber(array, number) {
-    function isTrue(number) {
-        return number >= 0 && number <= 9 && typeof number === 'number';
-    }
+    
 
-    if (array.every(isTrue)) {
+    if (array.every(number >= 0 && number <= 9 && typeof number === 'number')) {
         if (number >= 0 && Number.isInteger((number))) {
             let string = array.join(' ');
             string += number;
