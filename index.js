133ec09f4026948ac56e4cb61fa0c6bf07147c15
sangin <khidirovs1999@mail.ru>
2019-10-14 13:54:56 +0500

    asd

--- index.js
+++ index.js
@@ -3,7 +3,22 @@
  * @param number – число, которое нужно прибавить к числу, полученному из массива
 */
 function addNumber(array, number) {
-    // Решение задачи
+    let pow = 0;
+    let sum = 0;
+    let funcArray = function(item) {
+        return (item < 0 || item > 9 || typeof item !== "number")
+    }
+    if (array.some(funcArray)) {
+        return null;
+    }
+    array = array.reverse();
+    array.forEach(function(item) {
+        sum += item * Math.pow(10, pow);
+        pow++;
+    });
+    array = (number + sum).toString().split('');
+    
+    return array.map(Number);
 }
 
 module.exports = {
