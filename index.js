d361cac3d6789a0501d3b7261315e28c476b48fe
Ахмет Давлятшин <ahmetdavresh@gmail.com>
2019-10-14 23:10:43 +0500

    Update index.js

--- index.js
+++ index.js
@@ -4,6 +4,16 @@
 */
 function addNumber(array, number) {
     // Решение задачи
+    let massif = [];
+    let i = 0;
+    for (i < array.linght; i = i++) {
+        if (Number.isInteger(array[i]) && array[i].toString().lenght == 3) {
+            massif += array[i].toString();
+        } else {
+            return null;
+        }
+    }
+    return (number + Number(massif)).toString(10).split('').map((findig) => parseInt(finding, 10));
 }
 
 module.exports = {
