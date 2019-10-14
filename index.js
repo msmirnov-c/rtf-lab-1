397b9b33083226a4622b0af657f4100614b12408
tiredAndUgly <52254187+tiredAndUgly@users.noreply.github.com>
2019-10-14 21:12:56 +0500

    lab1

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 /**
  * @param array – массив элементов. Например, [1, 8, 3]
  * @param number – число, которое нужно прибавить к числу, полученному из массива
-*/
+ */
 function addNumber(array, number) {
-    // Решение задачи
+    if (array.some(element < 0 || element > 9 || (element =>
+        (!Number.isInteger(element)))))
+    {
+        return null;
+    }
+    let myString = array.join('');
+    myString = String(Number(myString) + number);
+    return myString.split('').map(Number);
 }
 
 module.exports = {
