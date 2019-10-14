e80a1d9665824c577ee1ea98f2665ca2ab94c00b
Ольга Сосина <56439623+Svet1aya@users.noreply.github.com>
2019-10-14 12:22:58 +0500

    first task

--- index.js
+++ index.js
@@ -2,7 +2,6 @@
      * @param array – массив элементов. Например, [1, 8, 3]
      * @param number – число, которое нужно прибавить к числу, полученному из массива
      */
-
 function addNumber(array, number) {
         if (array.every((value) => Number.isInteger(value))) {
             const arrStr = parseInt(array.join(''), 10) + number;
@@ -14,4 +13,5 @@ return null;
 
 module.exports = {
     addNumber,
+
 };
