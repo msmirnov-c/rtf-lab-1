98f867fd91391d86f74caf3a77b631d68cb870ad
campusprod <56389275+campusprod@users.noreply.github.com>
2019-10-14 19:33:51 +0500

    Update index.js

--- index.js
+++ index.js
@@ -5,21 +5,21 @@
 */
 
 function addNumber(array, number) {
-    for (let i = 0; i < array.length; i++)
-{ if (number < 0 || array == null || number == null
-        || array.some((item) => item < 0 || item == null || !Number.isInteger(item)))
-return(null);
-}
-let value = array.join('');
-let sum = String(parseInt(value) + number);
-let array2 =[];
-array2 = sum.split('');
-let array3 =[];
-for (let j = 0; j < array2.length; j++)
-array3[j] = parseInt(array2[j]);
+        for (let i = 0; i < array.length; i++) {
+        if (number < 0 || array == null || number == null ||
+            array.some((item) => item < 0 || item == null || !Number.isInteger(item)))
+            return (null);
+    }
+    let value = array.join('');
+    let sum = String(parseInt(value) + number);
+    let array2 = [];
+    array2 = sum.split('');
+    let array3 = [];
+    for (let j = 0; j < array2.length; j++)
+        array3[j] = parseInt(array2[j]);
     return (array3);
 }
 
 module.exports = {
-    addNumber;
+    addNumber
 };
