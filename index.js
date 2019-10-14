8b8686c67b92269ee95214204dfeee0ce286bdcd
campusprod <56389275+campusprod@users.noreply.github.com>
2019-10-14 19:01:02 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 /**
+/* eslint linebreak-style: ["error", "windows"] */
  * @param array – массив элементов. Например, [1, 8, 3]
  * @param number – число, которое нужно прибавить к числу, полученному из массива
 */
+
 function addNumber(array, number) {
     for (let i = 0; i < array.length; i++)
 { if (number < 0 || array == null || number == null
         || array.some((item) => item < 0 || item == null || !Number.isInteger(item)))
-console.log(null);
+return(null);
 }
 let value = array.join('');
 let sum = String(parseInt(value) + number);
@@ -15,7 +17,7 @@ array2 = sum.split('');
 let array3 =[];
 for (let j = 0; j < array2.length; j++)
 array3[j] = parseInt(array2[j]);
-console.log(array3);
+return(array3);
 }
 
 module.exports = {
