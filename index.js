fc9159ce60aa3e55a8f897330ad15aec51d97aec
Ольга Сосина <56439623+Svet1aya@users.noreply.github.com>
2019-10-14 14:00:28 +0500

    first task

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
      * @param number – число, которое нужно прибавить к числу, полученному из массива
      */
 function addNumber(array, number) {
-    if (array.every((value) => typeof value ==='number' || value > 0 || value < 9)) {
+    if (array.every((value) => typeof value === 'number' || value > 0 || value < 9)) {
         const arrStr = parseInt(array.join(''), 10) + number;
 
         return arrStr.toString().split('').map(Number);
