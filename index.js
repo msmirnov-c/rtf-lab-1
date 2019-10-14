43ef592825f6ad0014c2f4bf82a2102789f361dd
RomanGrishin <56441172+RomanGrishin@users.noreply.github.com>
2019-10-14 21:41:59 +0500

    Update index.js

--- index.js
+++ index.js
@@ -2,25 +2,18 @@
  * @param array – массив элементов. Например, [1, 8, 3]
  * @param number – число, которое нужно прибавить к числу, полученному из массива
 */
-function addNumber(array, number) 
-{
-    for(let i = 0; i < array.lenght; i++ ) 
-    {
-
-     if(typeof array[i] !=='number') 
+function addNumber(array, number) {
+for(let i = 0; i < array.lenght; i++ ) {
+if(typeof array[i] !=='number')
      {
-
+         
       return null;
-     } 
-    } 
-
+    } } 
    const str = array.join('');
    const strValue = parseInt(str,10);
    const str = strstrValue + number;
    array = array.concat(strValue.splite(''));
-   return array;
-
-}
+   return array;}
  module.exports = {
       addNumber
-  };
+};
