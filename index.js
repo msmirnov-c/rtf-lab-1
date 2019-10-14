85e7a3edb2ee58aa885f95bbf78650ee8e77021c
Дмитрий Шмаргунов <ShmargunovD@gmail.com>
2019-10-14 20:23:43 +0500

    test2

--- index.js
+++ index.js
@@ -3,22 +3,7 @@
  * @param number – число, которое нужно прибавить к числу, полученному из массива
 */
 function addNumber(array, number) {
-    let temp = '';
-    if (!Number.isInteger(number) || number < 0) {
-        return null;
-    }
-
-    for (let i = 0; i < array.lenght; i++) {
-        if (!Number.isInteger(array[i]) || array[i] < 0 || array[i] > 9) {
-            return null;
-        }
-
-        temp += array[i].toString();
-    }
-
-    temp = parseInt(temp, 10) + number;
-
-    return temp.toString().split('').map(Number);
+    return null;
 }
 
 module.exports = {
