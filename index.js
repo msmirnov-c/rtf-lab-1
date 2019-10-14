656a80906ef4f6a81d78eb941feb517054a0f0d3
AlyonaTes <48356689+AlyonaTes@users.noreply.github.com>
2019-10-14 12:07:16 +0500

    Лабораторная работа 1

--- index.js
+++ index.js
@@ -3,9 +3,23 @@
  * @param number – число, которое нужно прибавить к числу, полученному из массива
 */
 function addNumber(array, number) {
-    // Решение задачи
+    var  final = Array()
+    var final = []
+    var i = 0;
+    do
+    {
+        if (array[i] === Math.ceil(array[i]) && array[i] >= 0 && array[i] < 10 && (typeof(array[i]) === 'number')) 
+        {
+            final += array[i];
+            i++;
+        }
+        else {
+            return null;
+        }
+    }
+    while (i < array.length)
+    return (number + parseInt(final)).toString(10).split('').map(item => parseInt(item));
 }
-
 module.exports = {
     addNumber
 };
