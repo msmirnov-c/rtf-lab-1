a04fcf8700d1562d6c325bd8f7df7a9c1ba776c4
Plifac <plifacplifac@gmail.com>
2019-10-14 16:57:42 +0500

    Update index.js
    8456234153124548646854

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 /**
  * @param array – массив элементов. Например, [1, 8, 3]
  * @param number – число, которое нужно прибавить к числу, полученному из массива
-*/
+ */
 function addNumber(array, number) {
     // решение
     let newNumber = 0;
     if (array.every(number >= 0 && number <= 9 && typeof number === 'number')) {
         if (number >= 0 && Number.isInteger((number))) {
             let string = array.join('');
-            newNumber = Number(string);
+            newNumber = +string;
             newNumber += number;
             return newNumber.toString.split('').map(Number);
         }
