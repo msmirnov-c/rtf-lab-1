ef0d4c47415a04a5aa9547cbdd5d2c7923ea5f92
Дмитрий Шмаргунов <ShmargunovD@gmail.com>
2019-10-14 18:58:46 +0500

    fix tab

--- index.js
+++ index.js
@@ -3,17 +3,18 @@
  * @param number – число, которое нужно прибавить к числу, полученному из массива
 */
 function addNumber(array, number) {
-	var temp;
-	if(!Number.isInteger(number) || number < 0){
-		return null;
-	}
-	array.forEach(function(element){
-		if(!Number.isInteger(element) || element < 0 || element > 9){
-			return null;
-		}
-		temp += element;
-	});
-	temp = parseInt(temp) + number;
+    let temp;
+    if (!Number.isInteger(number) || number < 0){
+        return null;
+    }
+	
+    array.forEach(function(element){
+        if (!Number.isInteger(element) || element < 0 || element > 9){
+            return null;
+        }
+        temp += element;
+    });
+    temp = parseInt(temp) + number;
     return temp.toString().split('').map(Number);
 }
 
