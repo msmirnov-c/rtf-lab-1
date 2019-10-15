3dc515b3feb2b7d50a3188d21fce19c1d515191c
Sangin-Khidirov <48213526+Sangin-Khidirov@users.noreply.github.com>
2019-10-15 12:39:09 +0500

    Update index.js

--- index.js
+++ index.js
@@ -5,14 +5,16 @@
 function addNumber(array, number) {
     function validation(item) {
         return (item < 0 || item > 9 || typeof item !== 'number');
-    };
+    }
     if (array.some(validation)) {
         return null;
     }
+
     let pow = array.length - 1;
-    let sum = array.reduce(function(sum, current) {
-        sum += current * Math.pow(10,pow);
+    const sum = array.reduce(function (sum, current) {
+        sum += current * 10 ** pow;
         pow--;
+
         return sum;
     }, 0);
     const stringArray = (number + sum).toString().split('');
