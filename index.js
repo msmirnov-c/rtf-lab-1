d72d3d345e3cbac0e977cc0381dc08e7cb4a1c98
Daniil Popov <p.danya@mail.ru>
2019-10-15 13:20:53 +0500

    fixed-bash(x2)

--- index.js
+++ index.js
@@ -4,15 +4,15 @@
 */
 function addNumber(array, number) {
     const allElementsAreNumbers = array.every((item) => typeof (item) === 'number');
-    if (!allElementsAreNumbers || array.length === 0) { 
-        return null; 
+    if (!allElementsAreNumbers || array.length === 0) {
+        return null;
     }
-    
+
     const origNum = parseInt(array.join(''), 10) + number;
     const numStr = origNum.toString();
     const stringArr = numStr.split('');
     
-    return stringArr.map((item) => parseInt((item), 10));;
+    return stringArr.map((item) => parseInt((item), 10));
 }
 
 module.exports = {
