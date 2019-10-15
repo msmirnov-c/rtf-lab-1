63100b5f5b360c3fa7d224e8cd2741d84c238b57
RomanGrishin <56441172+RomanGrishin@users.noreply.github.com>
2019-10-15 12:53:15 +0500

    Update index.js

--- index.js
+++ index.js
@@ -6,17 +6,17 @@
 function addNumber(array, number) {
     if (array === null || number < 0 || number === null) {
         return null;
-    }
-    else {
-
+    }else {
         for (let i = 0; i < array.lentgh; i++) {
             if (array[i] > 9 || array[i] < 0 || typeof array[i] !== 'number') {
                 return null;
             }
             array[i] = toString(array[i]);
         }
+
         const str = parseInt(array.join(''), 10) + number;
         const strRes = str.toString();
+        array.some(element);
 
         return strRes.split('').map(element).parseInt(element, 10);
     }
