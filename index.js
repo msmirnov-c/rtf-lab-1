1bd91ce9da5fa3e3b5b0de73e517bacfd6032ecd
RomanGrishin <56441172+RomanGrishin@users.noreply.github.com>
2019-10-15 13:11:00 +0500

    Update index.js

--- index.js
+++ index.js
@@ -8,11 +8,10 @@ function addNumber(array, number) {
         if (array === null || number < 0 || number === null || array[i] > 9 || array[i] < 0 || typeof array[i] !== 'number') {
             return null;
         }
-        array[i] = toString(array[i]);
     }
-
+    const str = array.toString();
     const str = parseInt(array.join(''), 10) + number;
-
-    return strRes = str.toString().split('').map(Number);
+    const strRes = str.toString().split('').map(Number);
+    return strRes;
 }
 module.exports = { addNumber };
