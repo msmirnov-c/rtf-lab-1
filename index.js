a87c0ba616043eb68b553cdf18796479d365515f
veetash <ansvet6799@mail.ru>
2019-10-15 17:35:47 +0500

    stroka ne 100+

--- index.js
+++ index.js
@@ -3,12 +3,15 @@
  * @param number – число, которое нужно прибавить к числу, полученному из массива
 */
 function addNumber(array, number) {
-    const checkIsTrue = array.every((element) => element >= 0 && element <= 9 && Number.isInteger(element))
+    const checkIsTrue = array.every((element) =>
+    element >= 0
+    && element <= 9
+    && Number.isInteger(element));
     if (checkIsTrue) {
         const string = array.reduce((str, element) => str + element, '');
         const num = parseInt(string, 10) + number;
         const lastNum = num.toString();
-        let arr = lastNum.split('');
+        const arr = lastNum.split('');
 
         return arr.map((item) => parseInt(item, 10));
     }
