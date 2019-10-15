3269febdc07a2690895784941ad9d156c59cee40
farid2509 <48219711+farid2509@users.noreply.github.com>
2019-10-15 21:39:35 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,26 +1,21 @@
 function digitFind(array, number) {
     let i = 1;
-    let digit = 0;
     for (let j = array.length - 1; j >= 0; j--) {
         if (array[j] > 9 || array[j] < 0 || !Number.isInteger(array[j])) {
             return null;
         }
 
-        digit += array[j] * i;
+        number += array[j] * i;
         i *= 10;
     }
 
-    return digit + number;
+    return number;
 }
 
 function addNumber(array, number) {
     const digit = digitFind(array, number);
 
-    if (digit == null) {
-        return null;
-    }
-
-    return digit.toString().split('').map(Number);
+    if digit === null ? null : digit.toString().split('').map(Number);
 }
 
 module.exports = {
