922f489cef65be6287175baf98cab29a578a905d
Максим Кузнецов <48288135+1max87@users.noreply.github.com>
2019-10-16 15:13:46 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,19 +1,22 @@
 function addNumber(array, number) {
-    let sumElement = '';   
+    let sumElement = '';
     for (let i = 0; i < array.length; i++) {
         if ((typeof array[i]) !== 'number' || array[i] < 0 || array[i] > 10) {
             return null;
         }
+
         sumElement += array[i];
     }
+
     sumElement = String(Number(sumElement) + number);
-    const strArr = sumElement.split('', sumElement.length);
+    const strArr = sumElement.split ('', sumElement.length);
     const numbArr = [];
     for (let i = 0; i < strArr.length; i++){
         numbArr[i] = Number(strArr[i]);
-    }    
+    }
+
     return numbArr;
-}  
+}
 module.exports = {
     addNumber,
-}
+};
