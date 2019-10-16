80dbb53bda81f1b0421c6eb2e7eb97b3ccd3f2b3
Максим Кузнецов <48288135+1max87@users.noreply.github.com>
2019-10-16 14:51:43 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,20 +1,20 @@
 function addNumber(array, number) {
     // Решение задачи
-    let sumElement = '';    
-    for (let i = 0; i < array.length; i++){
-        if ((typeof array[i]) !== 'number' || array[i]<0 || array[i]>10){
+    let sumElement = '';   
+    for (let i = 0; i < array.length; i++) {
+        if ((typeof array[i]) !== 'number' || array[i] < 0 || array[i] > 10) {
             return null;
-            }
-        sumElement=sumElement+array[i];
+        }
+        sumElement += array[i];
     }
-    sumElement = String(Number(sumElement)+number);
+    sumElement = String(Number(sumElement) + number);
     let strArr = sumElement.split('', sumElement.length);
     let numbArr = [];
     for (let i = 0; i < strArr.length; i++){
-        numbArr[i]=Number(strArr[i]);
-      }    
+        numbArr[i] = Number(strArr[i]);
+    }    
     return numbArr;
-  }
+}
   
 module.exports = {
     addNumber
