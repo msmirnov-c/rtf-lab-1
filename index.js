c4fe5d334f9925ffb9b626d7b486bd6ac5de15d5
88ORLOV <44868289+88ORLOV@users.noreply.github.com>
2019-10-16 19:47:46 +0500

    сделал --fix

--- index.js
+++ index.js
@@ -4,7 +4,7 @@
  * @param number – число, которое нужно прибавить к числу, полученному из массива
 */
 function addNumber(array, number) {
-    if (number > 0 && array.every((item) => item >= 0 && item < 10 && Number.isInteger(item))) {
+    if (number > 0 && array.every(item => item >= 0 && item < 10 && Number.isInteger(item))) {
         const myString = array.join('');
         const mySum = Number(myString) + number;
         const intToStr = String(mySum);
