908b0c1b745c1a4db5d75c70ff57039329baadfd
88ORLOV <44868289+88ORLOV@users.noreply.github.com>
2019-10-16 19:07:02 +0500

    добавлена скобка, закрывающая функцию

--- index.js
+++ index.js
@@ -1,19 +1,20 @@
+
 /**
  * @param array – массив элементов. Например, [1, 8, 3]
  * @param number – число, которое нужно прибавить к числу, полученному из массива
 */
 function addNumber(array, number) {
-    if (number > 0 && array.every((item) => item >= 0 && item < 10 && Number.isInteger(item))) {
+    if (number > 0 && array.every(item => item >= 0 && item < 10 && Number.isInteger(item))) {
         const myString = array.join('');
         const mySum = Number(myString) + number;
         const intToStr = String(mySum);
         const myResult = intToStr.split('').map(Number);
 
         return myResult;
+    }
+
+    return null;
 }
-
-        return null;
-
 module.exports = {
     addNumber,
 };
