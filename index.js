d1f27542cdda46311996d9f55b962017d971ae97
Vladimir <43116496+VladimirSTAR0911@users.noreply.github.com>
2019-10-16 13:40:57 +0500

    Update index.js

--- index.js
+++ index.js
@@ -3,20 +3,22 @@
  * @param number – число, которое нужно прибавить к числу, полученному из массива
 */
 function addNumber(array, number) {
-    if (typeof array !== 'object' && typeof number !== 'number') return null;
-
+    if (typeof array !== 'object' && typeof number !== 'number') {
+        return null;
+    }
+    
     let outNums = '';
     for (let i = 0; i < array.length; i++) {
         if ((array[i] < 0 || array[i] > 9) || typeof array[i] !== 'number') {
-            outNums = 'exit';
-
             return null;
         }
 
         outNums += array[i];
     }
-
-    return outNums === 'exit' ? null : Array.from(String(Number(outNums) + number)).map((arr) => Number(arr));
+    
+    let outArray = Array.from(String(Number(outNums) + number))
+    //Объявление переменных(когда этого можно избежать) не будет лишним в коде? В плане загрузки памяти, эффективности, скорости и т.д.
+    return outArray.map((el) => Number(el));
 }
 
 module.exports = {
