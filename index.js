a213b17daad4491b3d56ca86a3045184a9bd07be
kirillKurilov <56518322+kirillKurilov@users.noreply.github.com>
2019-10-18 04:49:36 +0500

    Update index.js

--- index.js
+++ index.js
@@ -6,13 +6,15 @@ function addNumber(array, number) {
     const right = (item) => (!Number.isInteger(number) || (typeof item === 'number'));
     if (!array.every(right)) {
         return null;
-   }
-   {
-    const str1 = array.reduce((arr, num) => arr + num, '');
-    const amount = +str1 + number;
-    const array1 = amount.toString().split('');
-    const result = (array1.map((correct) => parseInt((correct), 10)));
-    return result;
+    }
+    
+    {
+        const str1 = array.reduce((arr, num) => arr + num, '');
+        const amount = +str1 + number;
+        const array1 = amount.toString().split('');
+        const result = (array1.map((correct) => parseInt((correct), 10)));
+        
+        return result;
     }
 }
 module.exports = {
