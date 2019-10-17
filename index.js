da861e773f55ca0865d3f845ff7aa35120398997
LEXA-E6ATb <votencevaleksejj@gmail.com>
2019-10-17 17:15:16 +0500

    update
    добавил методы массива и изменил название переменной

--- index.js
+++ index.js
@@ -3,15 +3,11 @@
  * @param number – число, которое нужно прибавить к числу, полученному из массива
 */
 function addNumber(array, number) {
-    for (let i = 0; i < array.length; i++) {
-        if (!(typeof (array[i]) === 'number') || !(array[i] >= 0) || !(array[i] <= 9)) {
-            return null;
-        }
-    }
+    if (array.every(item) => (typeof (item) === 'number' && item >= 0 && item <= 9)) return null;
 
-    const result = (parseInt(array.join(''), 10) + number).toString();
+    const numberFromArray = (parseInt(array.join(''), 10) + number).toString();
 
-    return result.split('').map((item) => parseInt(item, 10));
+    return numberFromArray.split('').map((item) => parseInt(item, 10));
 }
 
 module.exports = {
