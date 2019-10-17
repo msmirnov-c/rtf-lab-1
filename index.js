a5680c8b3673f26bdaff6dc70fac530321e11d71
AlyonaTes <48356689+AlyonaTes@users.noreply.github.com>
2019-10-17 22:51:29 +0500

    Лабораторная №1
    Исправила ошибки стиля

--- index.js
+++ index.js
@@ -1,20 +1,18 @@
-/* 
+/**
  * @param array – массив элементов. Например, [1, 8, 3]
- * @param number – число, F
+ * @param number – число, которое нужно прибавить к числу, полученному из массива
 */
 function addNumber(array, number) {
-
-    let final = array.every((item) => typeof (item) === 'number');
+    const final = array.every((item) => typeof (item) === 'number');
     if (!final || array.length === 0) {
         return null;
     }
 
-    let numberFinal = number + parseInt(array.join(''), 10);
+    const numberFinal = number + parseInt(array.join(''), 10);
 
     return numberFinal.toString().split('').map(Number);
 }
 
-console.log(addNumber([1,2,3],5))
 module.exports = {
-    addNumber
+    addNumber,
 };
