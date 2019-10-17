d811f2adb8dd7b3c50192eea441c8e055466b52f
kirillKurilov <56518322+kirillKurilov@users.noreply.github.com>
2019-10-18 04:52:33 +0500

    Update index.js

--- index.js
+++ index.js
@@ -7,13 +7,11 @@ function addNumber(array, number) {
     if (!array.every(right)) {
         return null;
     }
-    
     {
         const str1 = array.reduce((arr, num) => arr + num, '');
         const amount = +str1 + number;
         const array1 = amount.toString().split('');
         const result = (array1.map((correct) => parseInt((correct), 10)));
-        
         return result;
     }
 }
