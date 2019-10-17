dd1e17388fbc41058312f5ac8fff169819948635
AndrewMarshal <37539223+AndrewMarshal@users.noreply.github.com>
2019-10-17 20:32:22 +0500

    Update index.js

--- index.js
+++ index.js
@@ -4,12 +4,14 @@
 */
 function addNumber(array, number) {
     let numLine = '';
+    var totalAmount = 0;
     var condition = array.every((item) => item === Math.ceil(item) && item >= 0 && item <= 9 && (typeof (item) === 'number'));
-    numLine = array.reduce((accumulator, item, _i) => accumulator + item, numLine);
+    numLine = array.reduce((accumulator, item) => accumulator + item, numLine);
 
     if (!condition) {
         return null;
     }
+
     totalAmount = number + parseInt(numLine, 10);
 
     return totalAmount.toString(10).split('').map((item) => parseInt(item, 10));
