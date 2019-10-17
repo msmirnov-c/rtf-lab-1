cf7b33cc417d1c3f9e95acadea09d2589fe8534e
slavker99 <48331491+slavker99@users.noreply.github.com>
2019-10-17 20:59:47 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 function addNumber(array, number) {
     let numFromArray = '';
     for (let i = 0; i < array.length; i++) {
-        if (((typeof array[i]) !== 'number')||((array[i]<0)||(array[i]>9))) {
+        if (((typeof array[i]) !== 'number') || ((array[i] < 0)||(array[i] > 9))) {
             return null;
         }
+
         numFromArray += String(array[i]);
     }
 
     const num = String(Number(numFromArray) + number);
-    let returnedArray = num.split('', array.length + number);
+    const returnedArray = num.split('', array.length + number);
     for (let i = 0; i < returnedArray.length; i++) {
         returnedArray[i] = Number(returnedArray[i]);
     }
