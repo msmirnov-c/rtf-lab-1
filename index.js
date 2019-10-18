25534be4240798f1e0fdcfbf66ef071b0b265699
kirillKurilov <56518322+kirillKurilov@users.noreply.github.com>
2019-10-18 15:40:57 +0500

    Update index.js

--- index.js
+++ index.js
@@ -3,16 +3,17 @@
  * @param number – число, которое нужно прибавить к числу, полученному из массива
 */
 function addNumber(array, number) {
-    const right = (item) => (!Number.isInteger(number) || (typeof item === 'number'));
+    const right = item => (!Number.isInteger(number) || (typeof item === 'number'));
     if (!array.every(right)) {
         return null;
     }
+
     {
         const str1 = array.reduce((arr, num) => arr + num, '');
         const amount = +str1 + number;
         const array1 = amount.toString().split('');
-        const result = (array1.map((correct) => parseInt((correct), 10)));
-        return result;
+
+        return array1.map(correct => parseInt((correct), 10));
     }
 }
 module.exports = {
