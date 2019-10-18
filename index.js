91b9cf8631756f3f18e3680a0ed0e22cb409bf1f
Pavel Botalovgit config --global user.name Pavel <56505117+FleeingTwo9@users.noreply.github.com>
2019-10-18 15:18:28 +0500

    Update index.js
    Первый for заменён на array.some
    Второй for заменён на array.reduce
    Также основной код был переработан в цепочки для краткости

--- index.js
+++ index.js
@@ -1,21 +1,17 @@
 /**
- * @param array – массив элементов. Например, [1, 8, 3]
+ * @param arr – массив элементов. Например, [1, 8, 3]
  * @param number – число, которое нужно прибавить к числу, полученному из массива
  */
 function addNumber(array, number) {
-    for (let i = 0; i < array.length; i++) {
-        if (!Number.isInteger(array[i]) || array[i] < 0 || array[i] > 9) {
-            return null;
-        }
+    if (array.some(item => !Number.isInteger(item) || item < 0 || item > 9)) {
+        return null;
     }
 
-    array.reverse();
-    let result = 0;
-    for (let i = 0; i < array.length; i++) {
-        result += 10 ** i * array[i];
-    }
-
-    return (result + number).toString().split('').map(Number);
+    return ((array.reverse()
+        .reduce((total, current, index) => total + 10 ** index * current)) + number)
+        .toString()
+        .split('')
+        .map(Number);
 }
 
 module.exports = {
