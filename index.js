22e4003066ec44d42e49f2baa169663f8a0af2d9
kozhezhopa <vladikkozh@gmail.com>
2019-10-19 23:40:20 +0500

    correction13
    я уже не понимаю, что ему не нравится

--- index.js
+++ index.js
@@ -10,8 +10,9 @@ function addNumber(array, number) {
     }
 
     const initialValue = '';
-    let arrayAsString = array.reduce((accumulator, currentValue) =>
-        accumulator + currentValue, initialValue);
+    let arrayAsString = array.reduce((accumulator, currentValue) => {
+        accumulator + currentValue
+    }, initialValue);
 
     arrayAsString = parseInt(arrayAsString, 10) + number;
 
