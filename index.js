77d6420e2bc723076bd3c1f16a86dfbec10447a8
Ivanich99 <45899932+Ivanich99@users.noreply.github.com>
2019-10-19 17:03:12 +0500

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,30 @@
  * @param number – число, которое нужно прибавить к числу, полученному из массива
 */
 function addNumber(array, number) {
-    // Решение задачи
+    let dlin = 0;
+    let i = array.lenght;
+    let itog = 0;
+    while (i > 0) 
+    { 
+        if ((array[i] < 0 || array[i] > 9) || (number < 0) || 
+        (typeof array[i]!='integer' || typeof number !='integer') ||
+        (array.lenght<1))
+        {
+            return null;
+        }
+    else
+        {
+            itog = itog + (array[i]*(10 ** (array.lenght-i)));
+        }
+        i--;
+    }
+    dlin = alert((itog).toString().length)
+    let arr = [];
+    for (let i=0;i<dlin-1;i++)
+        {
+            arr[i]=parseInt(itog/(10**(array.lenght-i-1)));
+        }
+    return arr;
 }
 
 module.exports = {
