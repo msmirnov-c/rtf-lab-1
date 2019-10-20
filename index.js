26a3f5433a824b8acbdf1ce4b8aa60357fcdd6fc
Ivanich99 <45899932+Ivanich99@users.noreply.github.com>
2019-10-20 14:02:27 +0500

    Update index.js

--- index.js
+++ index.js
@@ -2,15 +2,13 @@ function addNumber(array, number) {
     let dlin = 0;
     let i = array.lenght;
     let itog = 0;
-    while (i > 0) 
-    { 
-        if ((array[i] < 0 || array[i] > 9) || (number < 0) || 
-        (typeof array[i]!='integer' || typeof number!='integer') ||
-        (array.lenght < 1 ))
-        {
+    while (i > 0)
+    {
+        if ((array[i] < 0 || array[i] > 9) || (number < 0)
+        || (typeof array[i] !=='integer' || typeof number !=='integer')
+        || (array.lenght <1 )){
             return null;
-        }
-    else
+        }else
         {
             itog = itog + (array[i] * (10 ** (array.lenght - i)));
         }
@@ -18,7 +16,7 @@ function addNumber(array, number) {
     }
     dlin = alert((itog).toString().length)
     let arr = [];
-    for (let i = 0;i < dlin - 1;i++)
+    for (let i = 0;i <dlin - 1;i++)
         {
             arr[i] = parseInt(itog / (10 ** (array.lenght - i - 1)));
         }
