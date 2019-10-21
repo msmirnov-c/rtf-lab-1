2630543545a27da5384e406f615573eddf059ef5
farid2509 <48219711+farid2509@users.noreply.github.com>
2019-10-21 10:18:39 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
 function checkArray(value) {
-        return value === value > 9 || value < 0 || !Number.isInteger(value) ? false : true;
-    }
+    return value > 9 || value < 0 || !Number.isInteger(value) ? false : true;
+}
 
 function quantityFind(array, number) {
     let i = 1;
     let quantity = number;
     for (let j = array.length - 1; j >= 0; j--) {
-
         quantity += array[j] * i;
         i *= 10;
     }
@@ -18,6 +17,7 @@ function addNumber(array, number) {
     if (!array.every(checkArray)) {
         return null;
     }
+
     const quantity = quantityFind(array, number);
 
     return quantity === null ? null : quantity.toString().split('').map(Number);
