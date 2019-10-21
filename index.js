2e5cc9e3b75ee71d706003ec6126a0e3b52cba8c
Vladimir <43116496+VladimirSTAR0911@users.noreply.github.com>
2019-10-21 22:50:51 +0500

    Update index.js

--- index.js
+++ index.js
@@ -3,15 +3,16 @@
  * @param number – число, которое нужно прибавить к числу, полученному из массива
 */
 function addNumber(array, number) {
-    if (typeof array !== 'object' && typeof number !== 'number' ||
-    !array.every((item) => Number.isInteger(item) || (item < 0 && item > 9))) {
+    if (typeof array !== 'object' || typeof number !== 'number' ||
+        !array.every((item) => Number.isInteger(item) || (item < 0 && item > 9))) {
         return null;
     }
 
     let outNums = '';
     outNums = array.reduce((sum, item) => sum + item, 0);
 
-    let outArray = Array.from(String(Number(outNums) + number));
+    const outArray = Array.from(String(Number(outNums) + number));
+
     return outArray.map((el) => Number(el));
 }
 
