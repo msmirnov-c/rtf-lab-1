71d3b783ba7f2f9086125178a82947deeb25881b
farid2509 <48219711+farid2509@users.noreply.github.com>
2019-10-21 10:14:18 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
-function digitFind(array, number) {
+function checkArray(value) {
+        return value === value > 9 || value < 0 || !Number.isInteger(value) ? false : true;
+    }
+
+function quantityFind(array, number) {
     let i = 1;
     let quantity = number;
     for (let j = array.length - 1; j >= 0; j--) {
-        if (array[j] > 9 || array[j] < 0 || !Number.isInteger(array[j])) {
-            return null;
-        }
 
         quantity += array[j] * i;
         i *= 10;
@@ -14,9 +15,12 @@ function digitFind(array, number) {
 }
 
 function addNumber(array, number) {
-    const digit = digitFind(array, number);
+    if (!array.every(checkArray)) {
+        return null;
+    }
+    const quantity = quantityFind(array, number);
 
-    return digit === null ? null : digit.toString().split('').map(Number);
+    return quantity === null ? null : quantity.toString().split('').map(Number);
 }
 
 module.exports = {
