f8d6c8d94cca7f1c76750cd2e027bdaa2d640bbc
ToottiFroottiTitoto <56549951+ToottiFroottiTitoto@users.noreply.github.com>
2019-10-23 15:47:33 +0500

    first task

--- index.js
+++ index.js
@@ -3,7 +3,20 @@
  * @param number – число, которое нужно прибавить к числу, полученному из массива
 */
 function addNumber(array, number) {
-    // Решение задачи
+    let stri;
+    let numb;
+   for(let i = 0; i <= array.length; i++) {
+       if (!isNaN(array[i]) || array.length === 0 ){
+           return null;
+       }
+       stri = stri + (String(array[i]));
+   }
+   numb = String(number + Number(stri));
+   numb.split('');
+   for(let i = 0; i < numb.length; i++){
+       numb[i] = Number(numb[i]);
+   }
+   return numb;
 }
 
 module.exports = {
