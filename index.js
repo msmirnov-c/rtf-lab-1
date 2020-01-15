bef0db87636744a2fe9ba7af604a683819ea856f
Comrade DAR <ahmetdavresh@gmail.com>
2020-01-15 04:40:21 +0300

    up3

--- index.js
+++ index.js
@@ -3,17 +3,22 @@
  * @param number – число, которое нужно прибавить к числу, полученному из массива
  */
 function addNumber(array, number) {
-    function check(value) {
-        let result = true;
-        if (value < 0 || value > 9 || typeof value !== 'number') {
-            result = false;
-        }
-        return result;
-    }
-    if (!array.every(check)) {
+    if (!(typeof (number) === 'number' && number >= 0 && array.length > 0)) {
         return null;
     }
-    return (parseInt(array.join(''), 10) + number).toString().split('').map(Number);
+
+    if (!array.every((elem) => typeof (elem) === 'number' && elem >= 0 && elem <= 9)) {
+        return null;
+    }
+
+    let result = 0;
+    for (let i = 0; i < array.length; i++) {
+        result += array[i] * (10 ** (array.length - i - 1));
+    }
+
+    result += number;
+
+    return result.toString().split('').map(Number);
 }
 
 module.exports = {
